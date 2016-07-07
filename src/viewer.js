4b16106f0727e10ac1362f6d43a6da3686f859ea
afogelberg <andreas.fogelberg@vasteras.se>
2016-07-07 15:40:15 +0200

    Fixed zoomLabel problem #52

--- src/viewer.js
+++ src/viewer.js
@@ -144,8 +144,8 @@ function init (el, mapOptions){
         var zoomControl = new ol.control.Zoom({
             zoomInTipLabel: ' ',
             zoomOutTipLabel:' ',
-            zoomInLabel: $.parseHTML('<svg class="mdk-icon-fa-plus"><use xlink:href="css/svg/fa-icons.svg#fa-plus"></use></svg>'),
-            zoomOutLabel: $.parseHTML('<svg class="mdk-icon-fa-minus"><use xlink:href="css/svg/fa-icons.svg#fa-minus"></use></svg>')
+            zoomInLabel: $.parseHTML('<svg class="mdk-icon-fa-plus"><use xlink:href="css/svg/fa-icons.svg#fa-plus"></use></svg>')[0],
+            zoomOutLabel: $.parseHTML('<svg class="mdk-icon-fa-minus"><use xlink:href="css/svg/fa-icons.svg#fa-minus"></use></svg>')[0]
         });
         //Set map controls
         mapControls = [
