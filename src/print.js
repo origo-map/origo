28516256e51e1117a489fa0212dffd85df77ae23
afogelberg <andreas.fogelberg@vasteras.se>
2016-10-21 10:48:30 +0200

    Repaired logo in print-control

--- src/print.js
+++ src/print.js
@@ -5,102 +5,100 @@
 "use strict";
 
 var $ = require('jquery');
-var viewer = require('./viewer');
+var Viewer = require('./viewer');
 var utils = require('./utils');
+var template = require("./templates/print.handlebars");
 
-var settings = {
-    printWith: 800
-};
-var printButton;
-
+var printWith = 800;
+var $printButton;
+var attribution;
 var map;
 
-function init() {
-    map = viewer.getMap();
+function Init(opt_options) {
+  var options = opt_options || {};
+  attribution = options.attribution || '© Lantmäteriet Geodatasamverkan';
+  map = Viewer.getMap();
 
-    var el = utils.createListButton({
-        id: 'o-print',
-        iconCls: 'o-icon-fa-print',
-        src: 'css/svg/fa-icons.svg#fa-print',
-        text: 'Skriv ut'
-    });
-    $('#o-menutools').append(el);
-    printButton = $('#o-print-button');
-
-    bindUIActions();
+  render();
+  bindUIActions();
 }
+
+function render() {
+  var el = utils.createListButton({
+    id: 'o-print',
+    iconCls: 'o-icon-fa-print',
+    src: 'css/svg/fa-icons.svg#fa-print',
+    text: 'Skriv ut'
+  });
+  $('#o-menutools').append(el);
+  $printButton = $('#o-print-button');
+}
+
 function bindUIActions() {
-    printButton.on('click', function(e) {
-        $('#app-wrapper').append('<canvas id="o-print" style="display: none"></canvas>');
-        createImage();
-        e.preventDefault();
-    });
+  $printButton.on('click', function(e) {
+    $('#app-wrapper').append('<canvas id="o-print" style="display: none"></canvas>');
+    createImage();
+    e.preventDefault();
+  });
 }
+
 function imageToPrint(printCanvas) {
-    var imageCrop = new Image();
-    try {
-        imageCrop.src = printCanvas.get(0).toDataURL("image/png");
-    }
-    catch (e) {
-        console.log(e);
-    }
-    finally {
-        var pw = '<html><head><link href="img/print.css" rel="stylesheet"></head><body>';
-        pw += '<div class="o-print-logo"><img src="print/logo_print.png"></div>';
-        pw += '<div class="o-map-canvas"><img src="' + imageCrop.src + '"/></div>';
-        pw += '<div class="o-print-attribution">&copy&nbsp;Lantmäteriet&nbsp;Geodatasamverkan</div>'
-        pw += '</body></html>';
-        var logoUrl = '&quot;css/logo_print.png&quot;';
-        var printWindow = window.open('','','width=800,height=820');
-        printWindow.document.write(pw);
-        printWindow.document.close();
-        setTimeout(function() {
-            printWindow.print();
-            setTimeout(function(){
-                printWindow.close();
-                $('#o-print').remove();
-            }, 10);
-        },200);
-    }
+  var imageCrop = new Image();
+  try {
+    imageCrop.src = printCanvas.get(0).toDataURL("image/png");
+  } catch (e) {
+    console.log(e);
+  } finally {
+    var templateOptions = {};
+    templateOptions.src = imageCrop.src;
+    templateOptions.attribution = attribution;
+    var pw = template(templateOptions);
+    var printWindow = window.open('', '', 'width=800,height=820');
+    printWindow.document.write(pw);
+    printWindow.document.close();
+    setTimeout(function() {
+      printWindow.print();
+      setTimeout(function() {
+        printWindow.close();
+        $('#o-print').remove();
+      }, 10);
+    }, 200);
+  }
 }
+
 function createImage() {
-    var canvas = $('canvas');
-    var image = new Image();
-    // image.crossOrigin = 'Anonymous';
+  var canvas = $('canvas');
+  var image = new Image();
 
-    try {
-        var imageUrl = canvas.get(0).toDataURL("image/png");
-    }
-    catch (e) {
-        console.log(e);
-    }
-    finally {
-      // printCanvas = copy of original map canvas
-      var printCanvas = $('#o-print');
-      image.onload = function() {
-          var ctxCanvas = printCanvas[0].getContext('2d');
-          var sourceWidth = canvas[0].width; //width of map canvas
-          var sourceHeight = canvas[0].height; //height of map canvas
-          //set the width of print canvas
-          if (sourceWidth < settings.printWith) {
-            printCanvas[0].width = sourceWidth;
-          }
-          else if (sourceWidth >= settings.printWith) {
-            printCanvas[0].width = settings.printWith;
-          }
-          //set the height of print canvas
-          if (sourceHeight < settings.printWith) {
-            printCanvas[0].height = sourceHeight;
-          }
-          else if (sourceWidth >= settings.printWith) {
-            printCanvas[0].height = settings.printWith;
-          }
+  try {
+    var imageUrl = canvas.get(0).toDataURL("image/png");
+  } catch (e) {
+    console.log(e);
+  } finally {
+    // printCanvas = copy of original map canvas
+    var printCanvas = $('#o-print');
+    image.onload = function() {
+      var ctxCanvas = printCanvas[0].getContext('2d');
+      var sourceWidth = canvas[0].width; //width of map canvas
+      var sourceHeight = canvas[0].height; //height of map canvas
+      //set the width of print canvas
+      if (sourceWidth < printWith) {
+        printCanvas[0].width = sourceWidth;
+      } else if (sourceWidth >= printWith) {
+        printCanvas[0].width = printWith;
+      }
+      //set the height of print canvas
+      if (sourceHeight < printWith) {
+        printCanvas[0].height = sourceHeight;
+      } else if (sourceWidth >= printWith) {
+        printCanvas[0].height = printWith;
+      }
 
-          ctxCanvas.drawImage(image, (sourceWidth/2 - printCanvas[0].width/2), 0, printCanvas[0].width, printCanvas[0].height, 0, 0, printCanvas[0].width, printCanvas[0].height);
-          imageToPrint(printCanvas);
-      };
-      image.src = imageUrl;
-    }
+      ctxCanvas.drawImage(image, (sourceWidth / 2 - printCanvas[0].width / 2), 0, printCanvas[0].width, printCanvas[0].height, 0, 0, printCanvas[0].width, printCanvas[0].height);
+      imageToPrint(printCanvas);
+    };
+    image.src = imageUrl;
+  }
 }
 
-module.exports.init = init;
+module.exports.init = Init;
