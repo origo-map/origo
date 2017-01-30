f681dce30243f56fcf3cd1b8cc18882274891842
afogelberg <andreas.fogelberg@vasteras.se>
2017-01-30 16:33:28 +0100

    Separated layer creation from viewer

---
+++ src/layer/tile.js
@@ -0,0 +1,12 @@
+/* ========================================================================
+ * Copyright 2016 Origo
+ * Licensed under BSD 2-Clause (https://github.com/origo-map/origo/blob/master/LICENSE.txt)
+ * ======================================================================== */
+"use strict";
+
+var ol = require('openlayers');
+
+module.exports = function vector(options, source) {
+  options.source = source;
+  return new ol.layer.Tile(options);
+}
