e1cd6a379097a0ba2cc3c12f7276ff2c9d919ae2
afogelberg <andreas.fogelberg@vasteras.se>
2017-02-23 16:40:00 +0100

    Finished centerposition

--- src/centerposition.js
+++ src/centerposition.js
@@ -14,24 +14,32 @@ var markerId = 'o-centerposition-marker';
 var resultId = 'o-centerposition-result';
 var toggleId = 'o-centerposition-toggle';
 var coordsId = 'o-centerposition-coords';
-var map;
-var view;
-var consoleId;
-var prefix;
-var suffix;
-var overlay;
+var map = undefined;
+var view = undefined;
+var consoleId = undefined;
+var title = undefined;
+var suffix = undefined;
+var defaultProjection = undefined;
+var projections = undefined;
+var projectionCodes = undefined;
+var projection = undefined;
 
 function Init(opt_options) {
   var options = opt_options || {};
   suffix = options.suffix || '';
-  prefix = options.prefix || '';
+  title = options.title || '';
   map = Viewer.getMap();
   view = map.getView();
+  projection = view.getProjection();
   consoleId = Viewer.getConsoleId();
+  projections = options.projections || {};
+  defaultProjection = Viewer.getProjectionCode();
+  projectionCodes = Object.getOwnPropertyNames(projections);
+  projections[defaultProjection] = title;
 
   addListener();
   render();
-  // bindUIActions();
+  bindUIActions();
 }
 
 function render() {
@@ -39,15 +47,16 @@ function render() {
     'background-color: #000;',
     'color: #fff;',
     'font-size: 10px;',
-    'padding: 5px 8px;',
+    'line-height: 10px;',
+    'padding: 5px 15px;',
     'border-radius: 10px;',
     'display: inline-block;',
-    '"Helvetica Neue",sans-serif'
+    'font-family: Arial, \'Helvetica Neue\', sans-serif;'
   ].join(' ');
-  var toggleButton = utils.createElement('button', prefix, {
+  var toggleButton = utils.createElement('button', title, {
     id: toggleId,
     style: buttonStyle,
-    value: prefix
+    value: defaultProjection
   });
   var coordsDiv = utils.createElement('div', '', {
     id: coordsId,
@@ -60,6 +69,16 @@ function render() {
   $('#' + consoleId).append(resultContainer);
 }
 
+function bindUIActions() {
+  $('#' + toggleId).on('click', function(e) {
+    var proj = toggleProjectionVal(this.value);
+    updateResult(proj);
+    onChangeCenter();
+    this.blur();
+    e.preventDefault();
+  });
+}
+
 function renderMarker() {
   var markerStyle = [
     'background-color: rgba(255,255,255,0.4);',
@@ -68,7 +87,13 @@ function renderMarker() {
     'line-height: 2rem;',
     'width: 2rem;',
     'height: 2rem;',
-    'text-align: center;'
+    'text-align: center;',
+    'top: 50%;',
+    'left: 50%;',
+    'margin-top: -1rem;',
+    'margin-left: -1rem;',
+    'z-index: 10000;',
+    'position: absolute;',
   ].join(' ');
   var marker = utils.createElement('div', '+', {
     id: markerId,
@@ -77,19 +102,33 @@ function renderMarker() {
   $('#o-map').append(marker);
 }
 
-function bindUIActions() {
-
-}
-
 function addListener() {
   view.on('change:center', onChangeCenter);
   map.on('movestart', onMoveStart);
-  map.on('moveend', onMoveEnd);
+}
+
+function toggleProjectionVal(val) {
+  var index;
+  var proj = undefined;
+  if (projectionCodes.length) {
+    index = projectionCodes.indexOf(val);
+    if (index === -1) {
+      proj = projectionCodes[0];
+    } else if (index === projectionCodes.length -1) {
+      proj = defaultProjection;
+    } else if (index < projectionCodes.length -1) {
+      proj = projectionCodes[index + 1];
+    }
+    return proj;
+  } else {
+    return val;
+  }
 }
 
 function onChangeCenter() {
   if (isPanning === false) {
     map.dispatchEvent('movestart');
+    map.on('moveend', onMoveEnd);
   } else {
     onMoving();
   }
@@ -98,35 +137,54 @@ function onChangeCenter() {
 function onMoveStart() {
   isPanning = true;
   renderMarker();
-  overlay = createOverlay(getCenter(), document.getElementById(markerId));
-  map.addOverlay(overlay);
 }
 
 function onMoveEnd() {
   isPanning = false;
-  map.removeOverlay(overlay);
   $('#' + markerId).remove();
+  map.un('moveend', onMoveEnd);
 }
 
 function onMoving() {
-  var coord = getCenter();
-  var center = coord.join(', ') + suffix;
+  var proj = $('#' + toggleId).val();
+  updateCoords(proj, view.getCenter());
+}
+
+function round(coords, decimals) {
+  if (decimals) {
+    return coords.map(function(coord) {
+      return coord.toFixed(decimals);
+    });
+  } else {
+    return coords.map(function(coord) {
+      return Math.round(coord);
+    });
+  }
+}
+
+function updateResult(proj) {
+  $('#' + toggleId).val(proj);
+  $('#' + toggleId).text(projections[proj]);
+  updateCoords(proj, view.getCenter());
+}
+
+function updateCoords(proj, sourceCoords) {
+  var coords = sourceCoords;
+  var geometry;
+  var center;
+  if (proj !== defaultProjection) {
+    geometry = new ol.Feature({
+      geometry: new ol.geom.Point(coords)
+    }).getGeometry();
+    coords = geometry.transform(projection, proj).getCoordinates();
+  }
+  if (proj === 'EPSG:4326') {
+    coords = round(coords, 5);
+  } else {
+    coords = round(coords);
+  }
+  center = coords.join(', ') + suffix;
   $('#' + coordsId).html(center);
-  overlay.setPosition(coord);
-}
-
-function createOverlay(coord, el) {
-  return new ol.Overlay({
-    position: coord,
-    element: el,
-    positioning: 'center-center'
-  });
-}
-
-function getCenter() {
-  return view.getCenter().map(function(coord) {
-    return Math.round(coord);
-  });
 }
 
 module.exports.init = Init;
