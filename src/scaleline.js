e0c2a1246d9b9eafe4c0c64f3b9d18a332cd457f
Andreas Fogelberg <andreas.fogelberg@gmail.com>
2017-03-16 13:31:26 +0100

    Added toolbars for control target (#210)

--- src/scaleline.js
+++ src/scaleline.js
@@ -11,10 +11,11 @@ var map;
 
 function Init(opt_options) {
   var options = opt_options || {};
+  var target = options.target || 'o-tools-bottom';
   map = Viewer.getMap();
 
   var scaleLine = new ol.control.ScaleLine({
-    target: 'o-bottom-tools'
+    target: target
   });
   map.addControl(scaleLine);
 }
