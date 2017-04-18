ce61db1080bbc81ee55dd185ea69127f9a51280b
Andreas Fogelberg <andreas.fogelberg@gmail.com>
2017-04-18 10:33:44 +0200

    Simplified geoposition (#201)

--- src/geoposition.js
+++ src/geoposition.js
@@ -11,13 +11,11 @@ var utils = require('./utils');
 
 var $geolocateButtonId = undefined;
 var $geolocateButton = undefined;
-var deltaMean = 500;
 var enabled = false;
 var map = undefined;
 var geolocation = undefined;
 var marker = undefined;
 var markerEl = undefined;
-var positions = undefined;
 var baseUrl = undefined;
 
 function init(opt_options) {
@@ -37,8 +35,6 @@ function init(opt_options) {
     element: markerEl,
     stopEvent: false
   });
-
-  positions = new ol.geom.LineString([], ('XYZM'));
 
   geolocation = new ol.Geolocation(({
     projection: map.getView().getProjection(),
@@ -85,7 +81,6 @@ function toggle() {
     geolocation.setTracking(false);
 
     geolocation.un('change', updatePosition);
-    map.un('postcompose', renderMap);
     map.removeOverlay(marker);
   } else {
     $geolocateButton.addClass('o-geolocation-button-true');
@@ -94,8 +89,6 @@ function toggle() {
     // Listen to position changes
     geolocation.on('change', updatePosition);
     geolocation.setTracking(true); // Start position tracking
-    map.on('postcompose', renderMap);
-    map.render();
   }
 }
 
@@ -114,67 +107,16 @@ function getPositionVal() {
 }
 
 function addPosition(current) {
-  var x = current.position[0];
-  var y = current.position[1];
-  var fCoords = positions.getCoordinates();
-  var previous = fCoords[fCoords.length - 1];
-  var prevHeading = previous && previous[2];
-  var previousM;
-  if (prevHeading) {
-    var headingDiff = current.heading - mod(prevHeading);
+  var position = current.position;
 
-    // force the rotation change to be less than 180°
-    if (Math.abs(headingDiff) > Math.PI) {
-      var sign = (headingDiff >= 0) ? 1 : -1;
-      headingDiff = -sign * (2 * Math.PI - Math.abs(headingDiff));
-    }
-    var heading = prevHeading + headingDiff;
+  if (enabled === false && geolocation.getTracking()) {
+    marker.setPosition(position);
+    map.getView().setCenter(position);
+    map.getView().setZoom(10);
+    enabled = true;
+  } else if (geolocation.getTracking()) {
+    marker.setPosition(position);
   }
-  positions.appendCoordinate([x, y, current.heading, current.m]);
-
-  // only keep the 20 last coordinates
-  positions.setCoordinates(positions.getCoordinates().slice(-20));
-
-  // FIXME use speed instead
-  if (current.heading && current.speed) {
-    markerEl.src = baseUrl + 'img/geolocation_marker_heading.png';
-  } else {
-    markerEl.src = baseUrl + 'img/geolocation_marker.png';
-  }
-
-  previousM = 0;
-
-  // change center and rotation before render
-  map.beforeRender(function(map, frameState) {
-    if (frameState !== null) {
-
-      // use sampling period to get a smooth transition
-      var m = frameState.time - deltaMean * 1.5;
-      m = Math.max(m, previousM);
-      previousM = m;
-
-      // interpolate position along positions LineString
-      var c = positions.getCoordinateAtM(m, true);
-      if (c && enabled === false && geolocation.getTracking()) {
-        marker.setPosition(c);
-        map.getView().setCenter(c);
-        map.getView().setZoom(10);
-        enabled = true;
-      } else if (c && geolocation.getTracking()) {
-        marker.setPosition(c);
-      }
-    }
-    return true; // Force animation to continue
-  });
-
-}
-
-function mod(n) {
-  return ((n % (2 * Math.PI)) + (2 * Math.PI)) % (2 * Math.PI);
-}
-
-function renderMap() {
-  map.render;
 }
 
 module.exports.init = init;
