60f2f78c86a26831b016cfe689aedabed9dd5b18
afogelberg <andreas.fogelberg@vasteras.se>
2017-06-09 16:16:15 +0200

    Added editable as an layer configuration

--- src/editor.js
+++ src/editor.js
@@ -1,6 +1,7 @@
  "use strict";
 
 var $ = require('jquery');
+var viewer = require('./viewer');
 var utils = require('./utils');
 var editorToolbar = require('./editor/editortoolbar');
 
@@ -15,6 +16,10 @@ module.exports = function() {
 
 function Init(opt_options) {
   var options = opt_options || {};
+  var editableLayers = viewer.getLayersByProperty('editable', true, true);
+  if (editableLayers.length) {
+    options.editableLayers = editableLayers;
+  }  
   options.autoSave = options.hasOwnProperty('autoSave') ? options.autoSave : true;
   options.currentLayer = options.defaultLayer || options.editableLayers[0];
   editorToolbar.init(options);
