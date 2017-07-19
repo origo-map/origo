6e9cda98ab015530d76b3069c0884a6372cd2c53
Markus <markus.meria@hallstahammar.se>
2017-07-19 14:07:04 +0200

    Replaced setcenter and setzoom with animate

--- src/geoposition.js
+++ src/geoposition.js
@@ -107,8 +107,7 @@ function addPosition(current) {
 
   if (enabled === false && geolocation.getTracking()) {
     marker.setPosition(position);
-    map.getView().setCenter(position);
-    map.getView().setZoom(10);
+    map.getView().animate({center: position}, {zoom: 10});
     enabled = true;
   } else if (geolocation.getTracking()) {
     marker.setPosition(position);
