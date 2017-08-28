1b17e9d2c8d519e110d9f676dffc1628302c92df
afogelberg <andreas.fogelberg@vasteras.se>
2017-08-28 11:06:52 +0200

    Made gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,65 @@
+var browserify = require('browserify');
+var sass = require('gulp-sass');
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var minifyCSS = require('gulp-csso');
+var runSequence = require('run-sequence');
+
+gulp.task('origo-debug', function () {
+  var b = browserify({
+    entries: 'origo.js',
+    debug: true,
+    standalone: 'origo'
+  });
+
+  return b.bundle()
+    .pipe(source('origo.js'))
+    .pipe(buffer())
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('origo-min', function () {
+  var b = browserify({
+    entries: 'origo.js',
+    debug: false,
+    standalone: 'origo'
+  });
+
+  return b.bundle()
+    .pipe(source('origo.js'))
+    .pipe(buffer())
+    .pipe(uglify({mangle: true}))
+    .pipe(rename('origo.min.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('origo-js', [ 'origo-debug', 'origo-min']);
+
+gulp.task('css-debug', function(){
+  return gulp.src('scss/origo.scss')
+    .pipe(sass())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('css-min', function(){
+  return gulp.src('scss/origo.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('origo-css', [ 'css-debug', 'css-min']);
 
 gulp.task('css', function(){
-  return gulp.src(['dist/style.css', 'css/print.css'])
+  return gulp.src(['dist/style.css', 'dist/style.min.css', 'css/print.css'])
     .pipe(gulp.dest('build/css'))
 });
 
@@ -39,4 +97,8 @@ gulp.task('root', function(){
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('pack', [ 'css', 'svg', 'png', 'img', 'js', 'examples', 'data', 'root']);
+gulp.task('pack', ['css', 'svg', 'png', 'img', 'js', 'examples', 'data', 'root']);
+
+gulp.task('default', function(cb) {
+  runSequence('origo-js', 'origo-css', 'pack');
+});
