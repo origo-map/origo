ea7635794e6cfd24a976858df9a17ef746629e08
jokd <jonas.kumlin@karlstad.se>
2017-11-02 12:04:37 +0100

    Colorbox integration

---
+++ src/utils/runfunction.js
@@ -0,0 +1,19 @@
+/* ========================================================================
+ * Copyright 2016 Origo
+ * Licensed under BSD 2-Clause (https://github.com/origo-map/origo/blob/master/LICENSE.txt)
+ * ======================================================================== */
+"use strict";
+
+var $ = require('jquery');
+var Colorbox = require('../../externs/jquery.colorbox');
+
+function showInfo(url, $this, evt) {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && url.indexOf(".pdf") > -1) {
+		var w = window.open(url, '_blank');
+	}
+	else {
+    $.colorbox({width:"95%", height:"95%", top: "0px", escKey: true, iframe:true, href:url});
+	}
+}
+
+module.exports.showInfo = showInfo;
