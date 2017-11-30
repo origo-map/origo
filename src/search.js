2b55d89aa4834e4744f3a3a6bbc5c1c72edb7653
Andreas Fogelberg <andreas.fogelberg@gmail.com>
2017-11-30 10:33:04 +0100

    Awesomplete (#358)
    * Replaced typeahead with awesomplete

--- src/search.js
+++ src/search.js
@@ -1,17 +1,28 @@
-"use strict";
+'use strict';
 
 var ol = require('openlayers');
 var $ = require('jquery');
 var Viewer = require('./viewer');
 var wktToFeature = require('./maputils').wktToFeature;
 var Popup = require('./popup');
-var typeahead = require('typeahead.js');
+var Awesomplete = require('awesomplete');
 var getFeature = require('./getfeature');
 var getAttributes = require('./getattributes');
 var featureInfo = require('./featureinfo');
 var mapUtils = require('./maputils');
 var getCenter = require('./geometry/getcenter');
 var utils = require('./utils');
+var generateUUID = require('./utils/generateuuid');
+var keyCodes = {
+  9: 'tab',
+  27: 'esc',
+  37: 'left',
+  39: 'right',
+  13: 'enter',
+  38: 'up',
+  40: 'down'
+};
+var searchDb = {};
 var map;
 var name;
 var northing;
@@ -26,15 +37,13 @@ var maxZoomLevel;
 var url;
 var title;
 var hintText;
-var hint;
-var highlight;
 var projectionCode;
 var overlay;
 var limit;
 var minLength;
+var awesomplete;
 
 function init(options) {
-  var el;
   name = options.searchAttribute;
   northing = options.northing || undefined;
   easting = options.easting || undefined;
@@ -52,16 +61,20 @@ function init(options) {
   maxZoomLevel = options.maxZoomLevel || Viewer.getResolutions().length - 2 || Viewer.getResolutions();
   limit = options.limit || 9;
   hintText = options.hintText || 'Sök...';
-  hint = options.hasOwnProperty('hint') ? options.hint : true;
-  highlight = options.hasOwnProperty('highlight') ? options.highlight : true;
   minLength = options.minLength || 4;
   projectionCode = Viewer.getProjectionCode();
 
   map = Viewer.getMap();
 
-  el = '<div id="o-search-wrapper">' +
+  render();
+  initAutocomplete();
+  bindUIActions();
+}
+
+function render() {
+  var el = '<div id="o-search-wrapper" class="o-search-wrapper">' +
     '<div id="o-search" class="o-search o-search-false">' +
-    '<input class="o-search-field typeahead form-control" type="text" placeholder="' + hintText + '">' +
+    '<input id="hjl" class="o-search-field form-control" type="text" placeholder="' + hintText + '">' +
     '<button id="o-search-button">' +
     '<svg class="o-icon-fa-search">' +
     '<use xlink:href="#fa-search"></use>' +
@@ -75,51 +88,165 @@ function init(options) {
     '</div>' +
     '</div>';
   $('#o-map').append(el);
-
-  // fix for internet explorer
-  $.support.cors = true;
-
-  $('.typeahead').typeahead({
-    autoSelect: true,
-    hint: hint,
-    highlight: highlight,
-    minLength: minLength
-  }, {
-    name: 'adress',
-    limit: limit,
-    displayKey: name,
-    source: function(query, syncResults, asyncResults) {
-      $.get(url + '?q=' + encodeURI(query), function(data) {
-        asyncResults(data);
-      });
-    }
-  });
-
-  bindUIActions();
 }
 
 function bindUIActions() {
-  $('.typeahead').on('typeahead:selected', selectHandler);
+  document.getElementById('hjl').addEventListener("awesomplete-selectcomplete", selectHandler);
 
   $('#o-search .o-search-field').on('input', function() {
-    if ($('#o-search .o-search-field.tt-input').val() && $('#o-search').hasClass('o-search-false')) {
+    if ($('#o-search .o-search-field').val() && $('#o-search').hasClass('o-search-false')) {
       $('#o-search').removeClass('o-search-false');
       $('#o-search').addClass('o-search-true');
       onClearSearch();
-    } else if (!($('#o-search .o-search-field.tt-input').val()) && $('#o-search').hasClass('o-search-true')) {
+    } else if (!($('#o-search .o-search-field').val()) && $('#o-search').hasClass('o-search-true')) {
       $('#o-search').removeClass('o-search-true');
       $('#o-search').addClass('o-search-false');
     }
   });
 }
 
+/*
+The label property in Awesomplete is used to store the feature id. This way the properties
+of each feature in the search response will be available in event handling.
+The complete properties are stored in a tempory db called searchDb. This is a workaround
+for a limit in Awesomplete that can only store data in the fields label and text.
+The data-catogry attribute is used to make a layer division in the sugguestion list.
+*/
+function initAutocomplete() {
+  var list;
+
+  $.support.cors = true;
+
+  var input = $("#o-search .o-search-field");
+  awesomplete = new Awesomplete('#o-search .o-search-field', {
+    minChars: minLength,
+    autoFirst: false,
+    sort: false,
+    item: renderList,
+    filter: function(suggestion, input) {
+      return suggestion.value;
+    }
+  });
+
+  var handler = function(data) {
+    list = [];
+    searchDb = {};
+    if (data.length) {
+      setSearchDb(data);
+      if (name && layerNameAttribute) {
+        list = groupToList(groupDb(searchDb));
+      } else {
+        list = dbToList(data);
+      }
+      awesomplete.list = list;
+      awesomplete.evaluate();
+    }
+  };
+
+  $(input).on("keyup", function(e) {
+    var keyCode = e.keyCode;
+    if (this.value.length >= minLength) {
+      if (keyCode in keyCodes) {
+        return;
+      } else {
+        makeRequest(handler, this);
+      }
+    }
+  });
+}
+
+function dbToList() {
+  var items = Object.keys(searchDb);
+  return items.map(function(item) {
+    return searchDb[item];
+  });
+}
+
+function groupDb(data) {
+  var group = {};
+  var ids = Object.keys(data);
+  ids.forEach(function(id) {
+    var item = data[id];
+    var type = item[layerNameAttribute];
+    var header = false;
+    if (type in group === false) {
+      group[type] = [];
+      item.header = Viewer.getLayer(type).get('title');
+    }
+    group[type].push(item);
+  });
+  return group;
+}
+
+function groupToList(group) {
+  var types = Object.keys(group);
+  var list = [];
+  var selection = {};
+  var nr = 0;
+  var turn = 0;
+  while (nr < limit && types.length) {
+    types.slice().forEach(function(type) {
+      if (nr < limit) {
+        var item = group[type][turn];
+        if (type in selection === false) {
+          selection[type] = [];
+        }
+        selection[type][turn] = item;
+        if (!group[type][turn + 1]) {
+          types.splice(types.indexOf(type), 1);
+        }
+      }
+      nr++;
+    });
+    turn++;
+  }
+  list = Object.keys(selection).reduce(function(previous, group) {
+    return previous.concat(selection[group]);
+  }, []);
+  return list;
+}
+
+function setSearchDb(data) {
+  data.forEach(function(item) {
+    var id = generateUUID();
+    item.label = id;
+    item.value = item[name];
+    searchDb[id] = item;
+  });
+}
+
+function renderList(suggestion, input) {
+  var item = searchDb[suggestion.label] || {};
+  var header = 'header' in item ? '<div class="heading">' + item.header + '</div>' : '';
+  var options = {};
+  var html = input === '' ? suggestion.value : suggestion.value.replace(RegExp(Awesomplete.$.regExpEscape(input), "gi"), "<mark>$&</mark>");
+  html = header + '<div class="suggestion">' + html + '</div>';
+  options = {
+    innerHTML: html,
+    'aria-selected': 'false'
+  };
+  if ('header' in item) {
+    options.className = 'header';
+  }
+
+  return Awesomplete.$.create("li", options);
+}
+
+function makeRequest(handler, obj) {
+  $.ajax({
+    url: url + '?q=' + encodeURI(obj.value),
+    type: 'GET',
+    dataType: 'json'
+  }).then(handler);
+}
+
 function onClearSearch() {
   $('#o-search-button-close').on('click', function(e) {
-    $('.typeahead').typeahead('val', '');
-    featureInfo.clear();
+    clearSearchResults();
+    clear();
     $('#o-search').removeClass('o-search-true');
     $('#o-search').addClass('o-search-false');
-    $('#o-search .o-search-field.tt-input').val('');
+    $('#o-search .o-search-field').val('');
     $('#o-search-button').blur();
     e.preventDefault();
   });
@@ -128,7 +255,6 @@ function onClearSearch() {
 function showOverlay(data, coord) {
   var popup;
   var content;
-  featureInfo.clear();
   clear();
   popup = Popup('#o-map');
   overlay = new ol.Overlay({
@@ -152,12 +278,21 @@ function showFeatureInfo(features, title, content) {
   obj.feature = features[0];
   obj.title = title;
   obj.content = content;
+  clear();
   featureInfo.identify([obj], 'overlay', getCenter(features[0].getGeometry()));
   mapUtils.zoomToExent(features[0].getGeometry(), maxZoomLevel);
 }
 
 function clear() {
-  Viewer.removeOverlays(overlay);
+  featureInfo.clear();
+  if (overlay) {
+    Viewer.removeOverlays(overlay);
+  }
+}
+
+function clearSearchResults() {
+  awesomplete.list = [];
+  setSearchDb([]);
 }
 
 /** There are several different ways to handle selected search result.
@@ -176,7 +311,9 @@ function clear() {
  * This is a simple single table search. In this case title, northing and easting
  * must be defined. */
 
-function selectHandler(evt, data) {
+function selectHandler(evt) {
+  var id = evt.text.label;
+  var data = searchDb[id];
   var layer;
   var id;
   var feature;
