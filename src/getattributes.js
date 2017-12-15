27dc5e032bd4f0ef13d8f8b3040a155efcdedf10
Jonas <jonas.kumlin@karlstad.se>
2017-12-15 12:36:45 +0100

    Option to open modal in full screen as iframe

--- src/getattributes.js
+++ src/getattributes.js
@@ -4,75 +4,74 @@ var replacer = require('../src/utils/replacer');
 var geom = require('./geom');
 
 module.exports = function(feature, layer) {
-    var content = '<div><ul>';
-    var attribute, li = '', title, val;
-    //If layer is configured with attributes
-    if(layer.get('attributes')) {
-          //If attributes is string then use template named with the string
-          if(typeof layer.get('attributes') === 'string') {
-              //Use attributes with the template
-              li = featureinfotemplates(layer.get('attributes'),feature.getProperties());
-          }
-          else {
-              for(var i=0; i<layer.get('attributes').length; i++) {
-                attribute = layer.get('attributes')[i];
-                title = '';
-                val = '';
-                if (attribute['name']) {
-                  if(feature.get(attribute['name'])) {
-                      val = feature.get(attribute['name']);
-                      if (attribute['title']) {
-                        title = '<b>' + attribute['title'] + '</b>';
-                      }
-                      if (attribute['url']) {
-                        if(feature.get(attribute['url'])) {
-                        var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(feature.get(attribute['url']), feature.getProperties()));
-                        val = '<a href="' + url + '" target="_blank">' +
-                              feature.get(attribute['name']) +
-                              '</a>';
-                        }
-                      }
-                  }
-                }
-                else if (attribute['url']) {
-                    if(feature.get(attribute['url'])) {
-                        var text = attribute['html'] || attribute['url'];
-                        var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(feature.get(attribute['url']), feature.getProperties()));
-                        val = '<a href="' + url + '" target="_blank">' +
-                              text +
-                              '</a>';
-                    }
-                }
-                else if (attribute['img']) {
-                    if(feature.get(attribute['img'])) {
-                        var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(feature.get(attribute['img']), feature.getProperties()));
-                        var attribution = attribute['attribution'] ? '<div class="o-image-attribution">' + attribute['attribution'] + '</div>' : '';
-                        val = '<div class="o-image-container">' +
-                                  '<img src="' + url + '">' + attribution +
-                              '</div>';
-                    }
-                }
-                else if (attribute['html']) {
-                  val = replacer.replace(attribute['html'], feature.getProperties(), {
-                    helper: geom,
-                    helperArg: feature.getGeometry()
-                  });
-                }
-
-                var cls = ' class="' + attribute['cls'] + '" ' || '';
-
-                li += '<li' + cls +'>' + title + val + '</li>';
-              }
-        }
-    }
-    else {
-      //Clean feature attributes from non-wanted properties
-      var attributes = filterObject(feature.getProperties(), ['FID_', 'geometry']);
+  var content = '<div><ul>';
+  var attribute, li = '', title, val, aCls;
+  //If layer is configured with attributes
+  if(layer.get('attributes')) {
+    //If attributes is string then use template named with the string
+    if(typeof layer.get('attributes') === 'string') {
       //Use attributes with the template
-      li = featureinfotemplates('default',attributes);
+      li = featureinfotemplates(layer.get('attributes'),feature.getProperties());
+    } else {
+      for(var i=0; i<layer.get('attributes').length; i++) {
+        attribute = layer.get('attributes')[i];
+        title = '';
+        val = '';
+        aCls = attribute['target'] === 'iframe' ? 'o-modal-link-iframe' : '';
+        if (attribute['name']) {
+          if(feature.get(attribute['name'])) {
+            val = feature.get(attribute['name']);
+            if (attribute['title']) {
+              title = '<b>' + attribute['title'] + '</b>';
+            }
+            if (attribute['url']) {
+              if(feature.get(attribute['url'])) {
+                var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(feature.get(attribute['url']), feature.getProperties()));
+              } else {
+                var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(attribute['url'], feature.getProperties()));
+              }
+              val = '<a href="' + url + '" target="_blank" class="' + aCls + '">' + feature.get(attribute['name']) + '</a>';
+            }
+          }
+        }
+        else if (attribute['url']) {
+          if(feature.get(attribute['url'])) {
+            var text = attribute['html'] || feature.get(attribute['url']);
+            var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(feature.get(attribute['url']), feature.getProperties()));
+            val = '<a href="' + url + '" target="_blank" class="' + aCls + '">' + text + '</a>';
+          } else {
+            var text = attribute['html'] || attribute['url'];
+            var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(attribute['url'], feature.getProperties()));
+            val = '<a href="' + url + '" target="_blank" class="' + aCls + '">' + text + '</a>';
+          }
+        }
+        else if (attribute['img']) {
+          if(feature.get(attribute['img'])) {
+            var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], replacer.replace(feature.get(attribute['img']), feature.getProperties()));
+            var attribution = attribute['attribution'] ? '<div class="o-image-attribution">' + attribute['attribution'] + '</div>' : '';
+            val = '<div class="o-image-container"><img src="' + url + '">' + attribution + '</div>';
+          }
+        }
+        else if (attribute['html']) {
+          val = replacer.replace(attribute['html'], feature.getProperties(), {
+            helper: geom,
+            helperArg: feature.getGeometry()
+          });
+        }
+
+        var cls = ' class="' + attribute['cls'] + '" ' || '';
+
+        li += '<li' + cls +'>' + title + val + '</li>';
+      }
     }
-    content += li + '</ul></div>';
-    return content;
+  } else {
+    //Clean feature attributes from non-wanted properties
+    var attributes = filterObject(feature.getProperties(), ['FID_', 'geometry']);
+    //Use attributes with the template
+    li = featureinfotemplates('default',attributes);
+  }
+  content += li + '</ul></div>';
+  return content;
 }
 function filterObject(obj, excludedKeys) {
     var result = {}, key;
--- src/modal.js
+++ src/modal.js
@@ -6,6 +6,7 @@ var modal = function() {
   var isStatic = undefined;
   var $target = undefined;
   var modalEl = undefined;
+  var extLink = undefined;
 
   return {
     createModal: createModal,
@@ -13,14 +14,23 @@ var modal = function() {
     closeModal: closeModal
   };
 
-  function render(title, content, cls) {
+  function render(title, content, cls, target) {
     modalEl = '<div id="o-modal" ' + 'class="' + cls + '">' +
-      '<div class="o-modal-screen"></div>' +
-      '<div class="o-modal">' +
-      '<div class="o-close-button"><svg class="o-icon-fa-times"><use xlink:href="#fa-times"></use></svg></div>' +
-      '<div class="o-modal-title">' + title + '</div>' +
-      '<div class="o-modal-content">' + content + '</div>' +
-      '</div>' +
+      '<div class="o-modal-screen"></div>';
+    if (target === 'iframe') {
+      modalEl +=  '<div class="o-modal-full"><div class="o-close-button"><svg class="o-icon-fa-times"><use xlink:href="#fa-times"></use></svg></div>' +
+        '<div class="o-external-link-button"><svg class="o-icon-fa-times"><use xlink:href="#fa-external-link"></use></svg></div>';
+      extLink = content;
+    } else {
+      modalEl +=  '<div class="o-modal"><div class="o-close-button"><svg class="o-icon-fa-times"><use xlink:href="#fa-times"></use></svg></div>';
+    }
+    modalEl +=  '<div class="o-modal-title">' + title + '</div>';
+    if (target === 'iframe') {
+      modalEl +=  '<iframe src="' + content + '" class="o-modal-content-full"></iframe>';
+    } else {
+      modalEl +=  '<div class="o-modal-content">' + content + '</div>';
+    }
+    modalEl +=  '</div>' +
       '</div>';
   }
 
@@ -34,18 +44,24 @@ var modal = function() {
         closeModal();
       });
     }
+    if(extLink){
+      $('.o-external-link-button').click(function() {
+        window.open(extLink, '_blank');
+      });
+    }
   }
 
   function createModal(modalTarget, options) {
     var title = options.title || '';
     var content = options.content || '';
     var cls = options.cls || '';
+    var target = options.target || '';
     if (options.hasOwnProperty('static')) {
       isStatic = options.static;
     } else {
       isStatic = false;
     }
-    render(title, content, cls);
+    render(title, content, cls, target);
     $target = $(modalTarget);
 
   }
