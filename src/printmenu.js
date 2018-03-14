ebbd101915b6fe9466d3f76c0d3c58b4062cb9c0
Magdalena Palm <magdalena.palm@sweco.se>
2018-03-14 11:33:01 +0100

    print contact och polygon

--- src/printmenu.js
+++ src/printmenu.js
@@ -3,63 +3,68 @@
 var $ = require('jquery');
 var utils = require('./utils');
 var options = require('./../conf/printSettings');
+var printarea = require('./utils/printarea');
+var Viewer = require('./viewer');
+var map;
+var layer;
 
-var $printMenu, $printButton, $printButtonTool;
+
+var $printMenu, $printButton, $printButtonTool, $printselect;
 
 function init() {
-    var menuEl = '<form type="submit">' + 
-                    '<div id="o-printmenu" class="o-printmenu">' +
-                        '<h5 id="o-main-setting-heading">Skriv ut karta</h5>' +                         
-                        '<div class="o-block">' +
-                            '<span class="o-setting-heading">Format</span>' +
-                            utils.createRadioButtons(options.formats, 1) +
-                        '</div>' +
-                        '<br />' +
-                        '<div class="o-block">' +
-                            '<span class="o-setting-heading">Orientering</span>' +
-                            utils.createRadioButtons(options.orientation, 2) +
-                        '</div>' +
-                        '</br>' +
-                        '<div class="o-block">' +
-                            '<span class="o-setting-heading">Storlek</span>' +
-                            '<select id="o-size-dd" class="o-dd-input">' +
-                            utils.createDdOptions(options.sizes) +                            
-                            '</select>' +
-                        '</div>' +
-                        '<div class="o-block">' +
-                            '<span class="o-setting-heading">Mall</span>' +
-                            '<select id="o-template-dd" class="o-dd-input">' +
-                            utils.createDdOptions(options.templates) +
-                            '</select>' +
-                        '</div>' +
-                        '<div class="o-block">' +
-                            '<span class="o-setting-heading">Skala</span>' +
-                            '<select id="o-scale-dd" class="o-dd-input">' +
-                            utils.createDdOptions(options.scales) +
-                            '</select>' +
-                        '</div>' +
-                        '<div class="o-block">' +
-                            '<span class="o-setting-heading">Upplösning</span>' +
-                            '<select id="o-resolution-dd" class="o-dd-input">' +
-                            utils.createDdOptions(options.resolutions) + 
-                            '</select>' +
-                        '</div>' +
-                        '<br />' +
-                        '<div class="o-block">' +
-                            '<span class="o-setting-heading">Titel<span><br />' +
-                            '<input id="o-title-input" class="o-text-input" type="text" />' +
-                        '</div>' +
-                        '<br />' +
-                        '<div class="o-block">' +
-                            '<input type="checkbox" id="o-legend-input" />' +
-                            '<label for="o-legend-input">Teckenförklaring</label>' +
-                        '</div>' +
-                        '<br />' +
-                        '<div class="o-block">' +
-                            '<button id="o-print-create-button" class="btn" type="submit">Skapa</button>' +
-                        '</div>' +
-                    '</div>' +
-                  '</form>';
+    var menuEl = '<form type="submit">' +
+        '<div id="o-printmenu" class="o-printmenu">' +
+        '<h5 id="o-main-setting-heading">Skriv ut karta</h5>' +
+        '<div class="o-block">' +
+        '<span class="o-setting-heading">Format</span>' +
+        utils.createRadioButtons(options.formats, 1) +
+        '</div>' +
+        '<br />' +
+        '<div class="o-block">' +
+        '<span class="o-setting-heading">Orientering</span>' +
+        utils.createRadioButtons(options.orientation, 2) +
+        '</div>' +
+        '</br>' +
+        '<div class="o-block">' +
+        '<span class="o-setting-heading">Storlek</span>' +
+        '<select id="o-size-dd" class="o-dd-input">' +
+        utils.createDdOptions(options.sizes) +
+        '</select>' +
+        '</div>' +
+        '<div class="o-block">' +
+        '<span class="o-setting-heading">Mall</span>' +
+        '<select id="o-template-dd" class="o-dd-input">' +
+        utils.createDdOptions(options.templates) +
+        '</select>' +
+        '</div>' +
+        '<div class="o-block">' +
+        '<span class="o-setting-heading">Skala</span>' +
+        '<select id="o-scale-dd" class="o-dd-input">' +
+        utils.createDdOptions(options.scales) +
+        '</select>' +
+        '</div>' +
+        '<div class="o-block">' +
+        '<span class="o-setting-heading">Upplösning</span>' +
+        '<select id="o-resolution-dd" class="o-dd-input">' +
+        utils.createDdOptions(options.resolutions) +
+        '</select>' +
+        '</div>' +
+        '<br />' +
+        '<div class="o-block">' +
+        '<span class="o-setting-heading">Titel<span><br />' +
+        '<input id="o-title-input" class="o-text-input" type="text" />' +
+        '</div>' +
+        '<br />' +
+        '<div class="o-block">' +
+        '<input type="checkbox" id="o-legend-input" />' +
+        '<label for="o-legend-input">Teckenförklaring</label>' +
+        '</div>' +
+        '<br />' +
+        '<div class="o-block">' +
+        '<button id="o-print-create-button" class="btn" type="button">Skapa</button>' +
+        '</div>' +
+        '</div>' +
+        '</form>';
 
     $('#o-map').append(menuEl);
 
@@ -69,37 +74,80 @@ function init() {
         iconCls: 'o-icon-menu-fa-times',
         src: '#fa-times',
         tooltipText: 'Stäng meny',
-        tooltipPlacement: 'west'        
+        tooltipPlacement: 'west'
     });
-    
-    $('#o-main-setting-heading').append(printButton);  
+
+    $('#o-main-setting-heading').append(printButton);
 
     var printButtonTool = utils.createButton({
         id: 'o-print-tool',
         iconCls: 'o-icon-fa-print',
-        src: '#fa-print'             
-    });    
+        src: '#fa-print'
+    });
 
-    $('#o-toolbar-misc').append(printButtonTool); 
-    $printButtonTool = $('#o-print-tool');    
+    $('#o-toolbar-misc').append(printButtonTool);
+    $printButtonTool = $('#o-print-tool');
     $printButton = $('#o-printmenu-button-close');
-    bindUIActions(); 
+    $printselect = $('#o-size-dd');
+    bindUIActions();
+
+
 
 }
 function bindUIActions() {
-    $printButton.on('click', function(e) {
-      $("#o-printmenu").removeClass('o-printmenu-show');
-      e.preventDefault();
-    });
-    $printButtonTool.on('click', function(e) {
-        if($("#o-printmenu").hasClass('o-printmenu-show')){
-            $("#o-printmenu").removeClass('o-printmenu-show');
-          }
-          else {
-            $("#o-printmenu").addClass('o-printmenu-show');
-          }
+    $printButton.on('click', function (e) {
+        $("#o-printmenu").removeClass('o-printmenu-show');
         e.preventDefault();
-      });
-  }
+    });
+    $printButtonTool.on('click', function (e) {
+        if ($("#o-printmenu").hasClass('o-printmenu-show')) {
+            $("#o-printmenu").removeClass('o-printmenu-show');
+        }
+        else {
+            $("#o-printmenu").addClass('o-printmenu-show');
+        }
+        e.preventDefault();
+    });
+    $printselect.change(function () {
+        if ($printselect.val() === "A1") {
+            printarea.printA1();
+        }
+    });  
+
+    $('#o-print-create-button').click(function (event) {  
+        map = Viewer.getMap();  
+        layer = Viewer.getMap()
+        .getLayers(); 
+        var printLayers = [];        
+        for (var i = 0, l = layer.a.length; i < l; i++) {
+           if(layer.a[i].S.visible){
+               printLayers.push(layer.a[i].S.title);
+           }         
+        }  
+        var contract = {
+            url: "http://localhost:8080/geoserver/wms",
+            dpi: $('#o-resolution-dd').val(),
+            layers: printLayers,
+            imageFormat: $('input[name=group1]:checked').val(),
+            scale: $('#o-scale-dd').val(),
+            orientation: $('input[name=group2]:checked').val(),
+            size: $('#o-size-dd').val(),
+            title: $('#o-title-input').val(),
+            template: $('#o-template-dd').val()
+        }; 
+        console.log("objekt", contract);
+        
+        
+        
+
+        
+        return false;
+    });
+
+
+}
+
+
+
 
 module.exports.init = init;
---
+++ src/utils/printarea.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var ol = require('openlayers');
+var $ = require('jquery');
+var Viewer = require('../viewer');
+var wgs84Sphere = new ol.Sphere(6378137);
+var map;
+var vector;
+var interaction;
+var target = '.o-map';
+var polygonFeature = new ol.Feature(
+    new ol.geom.Polygon([[[1499876.032897,  8295720.234419], [1498891.906158, 8295720.234419],
+      [1498844.133015, 8296981.445385], [1500363.318953, 8296952.781500]]]));    
+    
+
+function printA1(){
+    
+    map = Viewer.getMap();   
+    var style = new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 7,
+            fill: new ol.style.Fill({
+            color: [0, 153, 255, 1]
+          }),
+          stroke: new ol.style.Stroke({
+            color: [255, 255, 255, 0.75],
+            width: 1.5
+          })
+        }),
+        zIndex: 100000
+      });
+    
+    vector = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: [polygonFeature],
+            name: 'printarea',
+            visible: false,
+            zIndex: 7
+          }),
+          style: new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                width: 3,
+                color: [255, 0, 0, 1]
+              }),
+              fill: new ol.style.Fill({
+                color: [0, 0, 255, 0.6]
+              })
+          })
+    });   
+
+ 
+    var translate = new ol.interaction.Translate({
+        features: new ol.Collection([polygonFeature])
+    });
+    map.addLayer(vector);      
+    map.addInteraction(translate);
+};
+
+module.exports.printA1 = printA1;
+
+
