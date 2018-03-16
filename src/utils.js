8696f88d007141ae91e3d899343c9e328c851a59
sweco-seviax <victor.axelsson@sweco.se>
2018-03-16 10:17:29 +0100

    bugfix in createRadioButtons

--- src/utils.js
+++ src/utils.js
@@ -44,7 +44,7 @@ module.exports = {
      */
     createRadioButtons: function(options, group) {
         return options.map(function (rBtn, i) {
-            return '<label for="o-r' + i + '"><input type="radio" name="group'+ group + '" id="o-r' + i +'" value="'+ i + '" /> ' + rBtn + '</label><br />';
+            return '<label for="o-r' + i + group + '"><input type="radio" name="group'+ group + '" id="o-r' + i + group +'" value="'+ i + '" /> ' + rBtn + '</label><br />';
         }).toString().replace(new RegExp(',', 'g'), '');
     },
     createListButton: function(options) {
