67d9f2f52c195683d714e2fcd98ff0a2f9f1a97c
Daniel Rasmussen <dr865941@gmail.com>
2018-04-27 18:01:35 +0200

    fixes #443

---
+++ src/utils/round2.js
@@ -0,0 +1,10 @@
+module.exports = function round2(number, precision) {
+  var shift = function (number, precision, reverseShift) {
+    if (reverseShift) {
+      precision = -precision;
+    }
+    var numArray = ("" + number).split("e");
+    return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+  };
+  return shift(Math.round(shift(number, precision, false)), precision, true);
+};
