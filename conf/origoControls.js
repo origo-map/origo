f64154833b19b3e53e3ea6ac61dd67189016e789
afogelberg <andreas.fogelberg@vasteras.se>
2018-05-03 13:59:50 +0200

    Added controls

--- conf/origoControls.js
+++ conf/origoControls.js
@@ -1,47 +1,47 @@
 import geoposition from '../src/geoposition';
-// import mapmenu from '../src/mapmenu';
-// import print from '../src/print';
-// import sharemap from '../src/sharemap';
-// import legend from '../src/legend';
-// import search from '../src/search';
-// import editor from '../src/editor';
-// import home from '../src/home';
-// import measure from '../src/measure';
-// import about from '../src/about';
-// import scale from '../src/scale';
-// import offline from '../src/offline';
-// import link from '../src/link';
-// import scaleline from '../src/scaleline';
-// import zoom from '../src/zoom';
-// import rotate from '../src/rotate';
-// import attribution from '../src/attribution';
-// import position from '../src/position';
-// import fullscreen from '../src/fullscreen';
-// import splash from '../src/splash';
-// import draganddrop from '../src/draganddrop';
+import mapmenu from '../src/mapmenu';
+import print from '../src/print';
+import sharemap from '../src/sharemap';
+import legend from '../src/legend';
+import search from '../src/search';
+import editor from '../src/editor';
+import home from '../src/home';
+import measure from '../src/measure';
+import about from '../src/about';
+import scale from '../src/scale';
+import offline from '../src/offline';
+import link from '../src/link';
+import scaleline from '../src/scaleline';
+import zoom from '../src/zoom';
+import rotate from '../src/rotate';
+import attribution from '../src/attribution';
+import position from '../src/position';
+import fullscreen from '../src/fullscreen';
+import splash from '../src/splash';
+import draganddrop from '../src/draganddrop';
 
 const controls = {
-  geoposition //,
-  // mapmenu,
-  // print,
-  // sharemap,
-  // legend,
-  // search,
-  // editor,
-  // home,
-  // measure,
-  // about,
-  // scale,
-  // offline,
-  // link,
-  // scaleline,
-  // zoom,
-  // rotate,
-  // attribution,
-  // position,
-  // fullscreen,
-  // splash,
-  // draganddrop
+  geoposition,
+  mapmenu,
+  print,
+  sharemap,
+  legend,
+  search,
+  editor,
+  home,
+  measure,
+  about,
+  scale,
+  offline,
+  link,
+  scaleline,
+  zoom,
+  rotate,
+  attribution,
+  position,
+  fullscreen,
+  splash,
+  draganddrop
 };
 
 export default controls;
