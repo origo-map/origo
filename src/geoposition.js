eb6a595b3f7e1bc5213f174b5f444f7f74b1966a
tonnyandersson <tonnyandersson@users.noreply.github.com>
2018-05-23 17:52:13 +0200

    geoposition style guide fixes

--- src/geoposition.js
+++ src/geoposition.js
@@ -14,64 +14,6 @@ let baseUrl;
 let zoomLevel;
 let enabled = false;
 
-function init(opt_options) {
-  const options = opt_options || {};
-  const target = options.target || '#o-toolbar-navigation';
-  map = viewer.getMap();
-  baseUrl = viewer.getBaseUrl();
-  zoomLevel = options.zoomLevel || viewer.getResolutions().length - 3 || 0;
-
-  render(target);
-
-  $geolocateButtonId = $('#o-geolocation-button');
-  $geolocateButton = $('#o-geolocation-button button');
-
-  markerEl = $('#o-geolocation_marker').get(0);
-  marker = new Overlay({
-    positioning: 'center-center',
-    element: markerEl,
-    stopEvent: false
-  });
-
-  geolocation = new Geolocation(({
-    projection: map.getView().getProjection(),
-    trackingOptions: {
-      maximumAge: 10000,
-      enableHighAccuracy: true,
-      timeout: 600000
-    }
-  }));
-
-  bindUIActions();
-}
-
-function render(target) {
-  const tooltipText = 'Visa nuvarande position i kartan';
-  const src = `${baseUrl}img/geolocation_marker.png`;
-  const markerImg = `<img id="o-geolocation_marker" src="${src}"/>`;
-
-  // Element for control
-  const el = utils.createButton({
-    id: 'o-geolocation-button',
-    cls: 'o-geolocation-button',
-    iconCls: 'o-icon-fa-location-arrow',
-    src: '#fa-location-arrow',
-    tooltipText,
-    tooltipPlacement: 'east'
-  });
-  $(target).append(el);
-  $('#o-map').prepend(markerImg);
-}
-
-function bindUIActions() {
-  $geolocateButtonId.on('click', (e) => {
-    enabled = false;
-    toggle();
-    $geolocateButton.blur();
-    e.preventDefault();
-  });
-}
-
 function addPosition(current) {
   const position = current.position;
 
@@ -116,6 +58,64 @@ function toggle() {
     geolocation.on('change', updatePosition);
     geolocation.setTracking(true); // Start position tracking
   }
+}
+
+function render(target) {
+  const tooltipText = 'Visa nuvarande position i kartan';
+  const src = `${baseUrl}img/geolocation_marker.png`;
+  const markerImg = `<img id="o-geolocation_marker" src="${src}"/>`;
+
+  // Element for control
+  const el = utils.createButton({
+    id: 'o-geolocation-button',
+    cls: 'o-geolocation-button',
+    iconCls: 'o-icon-fa-location-arrow',
+    src: '#fa-location-arrow',
+    tooltipText,
+    tooltipPlacement: 'east'
+  });
+  $(target).append(el);
+  $('#o-map').prepend(markerImg);
+}
+
+function bindUIActions() {
+  $geolocateButtonId.on('click', (e) => {
+    enabled = false;
+    toggle();
+    $geolocateButton.blur();
+    e.preventDefault();
+  });
+}
+
+function init(optOptions) {
+  const options = optOptions || {};
+  const target = options.target || '#o-toolbar-navigation';
+  map = viewer.getMap();
+  baseUrl = viewer.getBaseUrl();
+  zoomLevel = options.zoomLevel || viewer.getResolutions().length - 3 || 0;
+
+  render(target);
+
+  $geolocateButtonId = $('#o-geolocation-button');
+  $geolocateButton = $('#o-geolocation-button button');
+
+  markerEl = $('#o-geolocation_marker').get(0);
+  marker = new Overlay({
+    positioning: 'center-center',
+    element: markerEl,
+    stopEvent: false
+  });
+
+  geolocation = new Geolocation(({
+    projection: map.getView().getProjection(),
+    trackingOptions: {
+      maximumAge: 10000,
+      enableHighAccuracy: true,
+      timeout: 600000
+    }
+  }));
+
+  bindUIActions();
 }
 
 export default { init };
