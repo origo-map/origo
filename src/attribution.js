30d0c665523051a1d249a7b137bdd15edb917360
tonnyandersson <tonnyandersson@users.noreply.github.com>
2018-05-23 16:10:35 +0200

    Attribution style guide fixes

--- src/attribution.js
+++ src/attribution.js
@@ -4,6 +4,13 @@ import viewer from './viewer';
 
 let map;
 
+function checkSize(attribution, breakPoint) {
+  const mapSize = map.getSize();
+  const collapsed = (mapSize[0] <= breakPoint[0] || mapSize[1] <= breakPoint[1]);
+  attribution.setCollapsible(collapsed);
+  attribution.setCollapsed(collapsed);
+}
+
 function init(opt) {
   const options = opt || {};
   const breakPoint = options.breakPoint || [768, 500];
@@ -15,15 +22,8 @@ function init(opt) {
 
   map.addControl(attribution);
 
-  $(window).on('resize', checkSize);
-  checkSize();
-
-  function checkSize() {
-    const mapSize = map.getSize();
-    const collapsed = (mapSize[0] <= breakPoint[0] || mapSize[1] <= breakPoint[1]);
-    attribution.setCollapsible(collapsed);
-    attribution.setCollapsed(collapsed);
-  }
+  $(window).on('resize', checkSize(attribution, breakPoint));
+  checkSize(attribution, breakPoint);
 }
 
 export default { init };
