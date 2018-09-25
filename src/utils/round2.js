f683ebe2baa9207a338113cf6c750a033f01e6d3
Rasmussen <daniel.rasmussen@vasteras.se>
2018-09-25 12:45:54 +0200

    updates #444 #443

--- src/utils/round2.js
+++ src/utils/round2.js
@@ -1,10 +1,13 @@
-module.exports = function round2(number, precision) {
-  var shift = function (number, precision, reverseShift) {
-    if (reverseShift) {
-      precision = -precision;
-    }
-    var numArray = ("" + number).split("e");
-    return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
-  };
+const shift = function shift(number, precision, reverseShift) {
+  let prec = precision;
+  if (reverseShift) {
+    prec = -prec;
+  }
+  const numArray = `${number}`.split('e');
+  return +`${numArray[0]}e${(numArray[1] ? (+numArray[1] + prec) : prec)}`;
+};
+const round2 = function round2(number, precision) {
   return shift(Math.round(shift(number, precision, false)), precision, true);
 };
+
+export default round2;
