83d88f26775602a4c6c241f5bb2689eb734ae04f
Jonas <jonas.kumlin@karlstad.se>
2018-09-27 12:37:58 +0200

    attribution

--- src/controls.js
+++ src/controls.js
@@ -1,5 +1,5 @@
 // export { default as About } from './controls/about';
-// export { default as Attribution } from './controls/attribution';
+export { default as Attribution } from './controls/attribution';
 // export { default as Draganddrop } from './controls/draganddrop';
 // export { default as Editor } from './controls/editor';
 export { default as Fullscreen } from './controls/fullscreen';
