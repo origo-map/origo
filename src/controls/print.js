4af86314a6136ef8754d16976f91e88d4b234425
Jonas <jonas.kumlin@karlstad.se>
2018-10-12 13:36:04 +0200

    Fix print.js

--- src/controls/print.js
+++ src/controls/print.js
@@ -1,106 +1,123 @@
-import $ from 'jquery';
-import viewer from '../viewer';
-import utils from '../utils';
+import cu from 'ceeu';
 import template from './print/printtemplate';
 
-let $printButton;
-let attribution;
-let baseUrl;
+const Print = function Print(options = {}) {
+  let {
+    attribution
+  } = options;
 
-function imageToPrint($printCanvas) {
-  const imageCrop = new Image();
-  try {
-    imageCrop.src = $printCanvas.get(0).toDataURL('image/png');
-  } catch (e) {
-    console.log(e);
-  } finally {
-    const templateOptions = {};
-    templateOptions.src = imageCrop.src;
-    templateOptions.attribution = attribution;
-    templateOptions.logoSrc = `${baseUrl}css/png/logo_print.png`;
-    const pw = template(templateOptions);
-    const printWindow = window.open('', '', 'width=800,height=820');
-    printWindow.document.write(pw);
-    printWindow.document.close();
-    setTimeout(() => {
-      printWindow.print();
+  let viewer;
+  let printButton;
+  let printElement;
+  let baseUrl;
+
+  function imageToPrint(printCanvasEl) {
+    const imageCrop = new Image();
+    try {
+      imageCrop.src = printCanvasEl.toDataURL('image/png');
+    } catch (e) {
+      console.log(e);
+    } finally {
+      const templateOptions = {};
+      templateOptions.src = imageCrop.src;
+      templateOptions.attribution = attribution;
+      templateOptions.logoSrc = `${baseUrl}css/png/logo_print.png`;
+      const pw = template(templateOptions);
+      const printWindow = window.open('', '', 'width=800,height=820');
+      printWindow.document.write(pw);
+      printWindow.document.close();
       setTimeout(() => {
-        printWindow.close();
-        $('#o-print').remove();
-      }, 10);
-    }, 200);
+        printWindow.print();
+        setTimeout(() => {
+          printWindow.close();
+          document.getElementById('o-print').remove();
+        }, 10);
+      }, 1200);
+    }
   }
-}
 
-function createImage() {
-  const $canvas = $('canvas');
-  const image = new Image();
-  let imageUrl;
+  function createImage() {
+    const canvasEl = document.getElementsByTagName('canvas');
+    const image = new Image();
+    let imageUrl;
 
-  try {
-    imageUrl = $canvas.get(0).toDataURL('image/png');
-  } catch (e) {
-    console.log(e);
-  } finally {
-    // $printCanvas = copy of original map canvas
-    const $printCanvas = $('#o-print');
-    image.onload = function imageonload() {
-      const printWidth = 800;
-      const ctxCanvas = $printCanvas[0].getContext('2d');
+    try {
+      imageUrl = canvasEl[0].toDataURL('image/png');
+    } catch (e) {
+      console.log(e);
+    } finally {
+      // $printCanvas = copy of original map canvas
+      const printCanvasEl = document.getElementById('o-print');
+      image.onload = function imageonload() {
+        const printWidth = 800;
+        const ctxCanvas = printCanvasEl.getContext('2d');
 
-      // width of map canvas
-      const sourceWidth = $canvas[0].width;
+        // width of map canvas
+        const sourceWidth = canvasEl[0].width;
 
-      // height of map canvas
-      const sourceHeight = $canvas[0].height;
+        // height of map canvas
+        const sourceHeight = canvasEl[0].height;
 
-      // set the width of print canvas
-      if (sourceWidth < printWidth) {
-        $printCanvas[0].width = sourceWidth;
-      } else if (sourceWidth >= printWidth) {
-        $printCanvas[0].width = printWidth;
-      }
+        // set the width of print canvas
+        if (sourceWidth < printWidth) {
+          printCanvasEl.width = sourceWidth;
+        } else if (sourceWidth >= printWidth) {
+          printCanvasEl.width = printWidth;
+        }
 
-      // set the height of print canvas
-      if (sourceHeight < printWidth) {
-        $printCanvas[0].height = sourceHeight;
-      } else if (sourceWidth >= printWidth) {
-        $printCanvas[0].height = printWidth;
-      }
+        // set the height of print canvas
+        if (sourceHeight < printWidth) {
+          printCanvasEl.height = sourceHeight;
+        } else if (sourceWidth >= printWidth) {
+          printCanvasEl.height = printWidth;
+        }
 
-      ctxCanvas.drawImage(image, ((sourceWidth / 2) - ($printCanvas[0].width / 2)), 0, $printCanvas[0].width, $printCanvas[0].height, 0, 0, $printCanvas[0].width, $printCanvas[0].height);
-      imageToPrint($printCanvas);
-    };
-    image.src = imageUrl;
+        ctxCanvas.drawImage(image, ((sourceWidth / 2) - (printCanvasEl.width / 2)), 0, printCanvasEl.width, printCanvasEl.height, 0, 0, printCanvasEl.width, printCanvasEl.height);
+        imageToPrint(printCanvasEl);
+      };
+      image.src = imageUrl;
+    }
   }
-}
 
-function render() {
-  const el = utils.createListButton({
-    id: 'o-print',
-    iconCls: 'o-icon-fa-print',
-    src: '#fa-print',
-    text: 'Skriv ut'
+  return cu.Component({
+    name: 'print',
+    onAdd(evt) {
+      viewer = evt.target;
+      baseUrl = viewer.getBaseUrl();
+      this.addComponents([printButton]);
+      this.render();
+    },
+    onInit() {
+      if (!attribution) attribution = '© Lantmäteriet Geodatasamverkan';
+
+      printButton = cu.Button({
+        id: 'o-print-button',
+        cls: 'o-menu-button',
+        click() {
+          const canvasEl = cu.dom.html('<canvas id="o-print" style="display: none"></canvas>');
+          document.getElementById('app-wrapper').append(canvasEl);
+          createImage();
+        },
+        text: 'Skriv ut',
+        icon: '#ic_print_24px',
+        iconCls: 'o-button-icon'
+      });
+
+      const rendered = printButton.render();
+
+      printElement = cu.Element({
+        cls: '',
+        tagName: 'li',
+        innerHTML: `${rendered}`
+      });
+    },
+    render() {
+      const htmlString = printElement.render();
+      const el = cu.dom.html(htmlString);
+      document.getElementById('o-menutools').appendChild(el);
+      this.dispatch('render');
+    }
   });
-  $('#o-menutools').append(el);
-  $printButton = $('#o-print-button');
-}
+};
 
-function bindUIActions() {
-  $printButton.on('click', (e) => {
-    $('#app-wrapper').append('<canvas id="o-print" style="display: none"></canvas>');
-    createImage();
-    e.preventDefault();
-  });
-}
-
-function init(opt) {
-  const options = opt || {};
-  attribution = options.attribution || '© Lantmäteriet Geodatasamverkan';
-  baseUrl = viewer.getBaseUrl();
-
-  render();
-  bindUIActions();
-}
-
-export default { init };
+export default Print;
