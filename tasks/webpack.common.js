8b798b3337826873856a1d6d7aeb2108da1b2e00
afogelberg <andreas.fogelberg@vasteras.se>
2018-11-08 09:00:10 +0100

    Changed browser version

--- tasks/webpack.common.js
+++ tasks/webpack.common.js
@@ -15,7 +15,7 @@ module.exports = {
           presets: [
             ['@babel/preset-env', {
               targets: {
-                browsers: ['ie >= 11']
+                browsers: ['chrome >= 39']
               },
               modules: false,
               useBuiltIns: 'entry'
