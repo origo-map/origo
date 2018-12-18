401ad874911519e5b31aac3dabcdb418a3562612
afogelberg <andreas.fogelberg@vasteras.se>
2018-12-18 17:43:12 +0100

    Included missing polyfill includes

--- src/utils/polyfill.js
+++ src/utils/polyfill.js
@@ -1,6 +1,7 @@
 import 'core-js/fn/string/ends-with';
 import 'core-js/fn/string/starts-with';
 import 'core-js/fn/array/find';
+import 'core-js/fn/array/includes';
 import 'core-js/fn/array/is-array';
 import 'core-js/fn/object/assign';
 import 'core-js/fn/object/keys';
