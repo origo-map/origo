25f0d12be5d93a56890ca395beab850187949d37
Iman Tahriri <iman_tahriri@yahoo.com>
2019-03-24 19:50:33 +0100

    make infowindow a draggable component

--- src/infowindow.js
+++ src/infowindow.js
@@ -11,8 +11,11 @@ let selectionManager;
 function render(viewerId) {
     mainContainer = document.createElement('div');
     mainContainer.classList.add('sidebarcontainer');
+    mainContainer.id = 'sidebarcontainer';
     urvalContainer = document.createElement('div');
     urvalContainer.classList.add('urvalcontainer');
+    // We add this so that urvalcontainer can become draggable
+    urvalContainer.id = 'sidebarcontainer-draggable';
     const urvalTextNodeContainer = document.createElement('div');
     urvalTextNodeContainer.classList.add('urval-textnode-container');
     const urvalTextNode = document.createTextNode('Urval');
@@ -50,6 +53,50 @@ function render(viewerId) {
     const parentElement = document.getElementById(viewerId);
     parentElement.appendChild(mainContainer);
     mainContainer.classList.add('hidden');
+
+    //Make the DIV element draggagle:
+    makeElementDraggable(mainContainer);
+}
+
+function makeElementDraggable(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "-draggable")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "-draggable").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
 
 function handleExport() {
