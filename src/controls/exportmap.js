3e4dff9c3b5514fb45c1d2f21a8899cdaccfb56a
Iman Tahriri <iman_tahriri@yahoo.com>
2019-04-01 20:14:13 +0200

    fixing group layers bug

--- src/controls/exportmap.js
+++ src/controls/exportmap.js
@@ -1,11 +1,10 @@
 import { Component, dom } from '../ui';
 
-const Exportmap = function (options = {}) {
-
+const Exportmap = function Exportmap(options = {}) {
   const {
     buttonText = 'Ladda ner kartan',
-    attributionFontSize = options.attributionFontSize = "10",
-    attributionFontColor = options.attributionFontColor = "rgb(64, 64, 64)",
+    attributionFontSize = options.attributionFontSize = '10',
+    attributionFontColor = options.attributionFontColor = 'rgb(64, 64, 64)',
     icon = '#fa-download'
   } = options;
 
@@ -15,89 +14,55 @@ const Exportmap = function (options = {}) {
   let map;
   let alreadyDrawn = false;
 
-  function download(format) {
-    const attr = getAttributions();
-    const scaleInfo = getScaleInfo();
-
-    map.once('postrender', (event) => {
-      let canvasOriginal = document.getElementsByTagName('canvas')[0];
-      // cloning canvas so that adding text to it doesn't dirty map view.
-      let canvas = cloneCanvas(canvasOriginal);
-      let ctx = canvas.getContext("2d");
-      //var text = ctx.measureText('foo'); // TextMetrics object
-      ctx.font = attributionFontSize + "px Arial";
-      ctx.fillStyle = attributionFontColor;
-      ctx.strokeStyle = attributionFontColor;
-
-      ctx.fillText(attr, 10, canvas.height - 5);
-
-      ctx.beginPath();
-      ctx.moveTo(canvas.width - 5, canvas.height - 10);
-      ctx.lineTo(canvas.width - 5, canvas.height - 5);
-      ctx.lineTo(canvas.width - 5 - scaleInfo.width, canvas.height - 5);
-      ctx.lineTo(canvas.width - 5 - scaleInfo.width, canvas.height - 10);
-
-      ctx.font = "10px Arial";
-      const textSize = ctx.measureText(scaleInfo.innerHTML); // TextMetrics object
-      ctx.fillText(scaleInfo.innerHTML, canvas.width - 5 - (scaleInfo.width / 2) - (textSize.width / 2), canvas.height - 10);
-      ctx.stroke();
-
-      let fileName = format === 'image/png' ? 'map.png' : 'map.jpeg';
-
-      canvas.toBlob((blob) => {
-        if (navigator.msSaveBlob) {
-          navigator.msSaveBlob(blob, fileName);
-        } else {
-          let link = document.createElement('a');
-          let objectURL = URL.createObjectURL(blob);
-          link.setAttribute('download', fileName);
-          link.setAttribute('href', objectURL);
-          link.click();
-          URL.revokeObjectURL(objectURL);
-        }
-      }, format);
-    });
-    map.renderSync();
-  }
-
   function cloneCanvas(oldCanvas) {
+    // create a new canvas
+    const newCanvas = document.createElement('canvas');
+    const context = newCanvas.getContext('2d');
 
-    //create a new canvas
-    let newCanvas = document.createElement('canvas');
-    let context = newCanvas.getContext('2d');
-
-    //set dimensions
+    // set dimensions
     newCanvas.width = oldCanvas.width;
     newCanvas.height = oldCanvas.height;
 
-    //apply the old canvas to the new one
+    // apply the old canvas to the new one
     context.drawImage(oldCanvas, 0, 0);
 
-    //return the new canvas
+    // return the new canvas
     return newCanvas;
   }
-
   function getAttributions() {
-
     const layers = viewer.getLayers();
-    const attributionsFunctions = layers.map(layer => {
-      if (layer.getVisible())
-        return layer.getSource().getAttributions();
+    const attributionsFunctions = [];
+
+    function getAttributionsFunction(layer) {
+      if (layer.getVisible()) {
+        // If layer type is "GROUP" then it is not a real layer and does not have any source, we need to check its sublayers.
+        if (layer.get('type') === 'GROUP') {
+          const subLayers = layer.getLayers();
+          subLayers.forEach(lyr => getAttributionsFunction(lyr));
+        } else {
+          attributionsFunctions.push(layer.getSource().getAttributions());
+        }
+      }
+    }
+
+    layers.forEach((layer) => {
+      getAttributionsFunction(layer);
     });
+
     const attributionHTMLs = [];
-    attributionsFunctions.forEach(f => {
-      if (f) { // check is needed because layers without attribution return null. And if layer is not visible returned value of the map is undefined.
-        const attributionsList = f();
+    attributionsFunctions.forEach((func) => {
+      if (func) { // check is needed because layers without attribution return null.
+        const attributionsList = func();
         attributionsList.forEach(att => attributionHTMLs.push(att));
       }
     });
 
-    const replacedAttributions = attributionHTMLs.map(a => {
+    const replacedAttributions = attributionHTMLs.map((a) => {
       // enkel lösning för att slippa anchor tag med länk till OSM
-      if (a.indexOf('OpenStreetMap') > 0)
+      if (a.indexOf('OpenStreetMap') > 0) {
         return '© OpenStreetMap';
-      else
-        return a.replace('&copy', '©');
+      }
+      return a.replace('&copy', '©');
     });
     return replacedAttributions.join(' ');
   }
@@ -113,6 +78,52 @@ const Exportmap = function (options = {}) {
     };
   }
 
+  function download(format) {
+    const attr = getAttributions();
+    const scaleInfo = getScaleInfo();
+
+    map.once('postrender', (event) => {
+      const canvasOriginal = document.getElementsByTagName('canvas')[0];
+      // cloning canvas so that adding text to it doesn't dirty map view.
+      const canvas = cloneCanvas(canvasOriginal);
+      const ctx = canvas.getContext('2d');
+      // var text = ctx.measureText('foo'); // TextMetrics object
+      ctx.font = `${attributionFontSize}px Arial`;
+      ctx.fillStyle = attributionFontColor;
+      ctx.strokeStyle = attributionFontColor;
+
+      ctx.fillText(attr, 10, canvas.height - 5);
+
+      ctx.beginPath();
+      ctx.moveTo(canvas.width - 5, canvas.height - 10);
+      ctx.lineTo(canvas.width - 5, canvas.height - 5);
+      ctx.lineTo(canvas.width - 5 - scaleInfo.width, canvas.height - 5);
+      ctx.lineTo(canvas.width - 5 - scaleInfo.width, canvas.height - 10);
+
+      ctx.font = '10px Arial';
+      const textSize = ctx.measureText(scaleInfo.innerHTML); // TextMetrics object
+      ctx.fillText(scaleInfo.innerHTML, canvas.width - 5 - (scaleInfo.width / 2) - (textSize.width / 2), canvas.height - 10);
+      ctx.stroke();
+
+      const fileName = format === 'image/png' ? 'map.png' : 'map.jpeg';
+
+      canvas.toBlob((blob) => {
+        if (navigator.msSaveBlob) {
+          navigator.msSaveBlob(blob, fileName);
+        } else {
+          const link = document.createElement('a');
+          const objectURL = URL.createObjectURL(blob);
+          link.setAttribute('download', fileName);
+          link.setAttribute('href', objectURL);
+          link.click();
+          URL.revokeObjectURL(objectURL);
+        }
+      }, format);
+    });
+    map.renderSync();
+  }
+
+
   /**
  * Using canvas.msToBlob() is much easier but it always turns a blob to a png.
  * This polyfill is needed if we want to choose format other than png.
@@ -120,14 +131,14 @@ const Exportmap = function (options = {}) {
   (function runPolyfills() {
     if (!HTMLCanvasElement.prototype.toBlob) {
       Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
-        value: function (callback, type, quality) {
-          var dataURL = this.toDataURL(type, quality).split(',')[1];
-          setTimeout(function () {
-            var binStr = atob(dataURL),
+        value(callback, type, quality) {
+          const dataURL = this.toDataURL(type, quality).split(',')[1];
+          setTimeout(() => {
+            let binStr = atob(dataURL),
               len = binStr.length,
               arr = new Uint8Array(len);
 
-            for (var i = 0; i < len; i++) {
+            for (let i = 0; i < len; i++) {
               arr[i] = binStr.charCodeAt(i);
             }
             callback(new Blob([arr], { type: type || 'image/png' }));
@@ -137,16 +148,16 @@ const Exportmap = function (options = {}) {
     }
     if (!Array.prototype.find) {
       Object.defineProperty(Array.prototype, 'find', {
-        value: function (predicate) {
+        value(predicate) {
           // 1. Let O be ? ToObject(this value).
           if (this == null) {
             throw new TypeError('"this" is null or not defined');
           }
 
-          var o = Object(this);
+          const o = Object(this);
 
           // 2. Let len be ? ToLength(? Get(O, "length")).
-          var len = o.length >>> 0;
+          const len = o.length >>> 0;
 
           // 3. If IsCallable(predicate) is false, throw a TypeError exception.
           if (typeof predicate !== 'function') {
@@ -154,10 +165,10 @@ const Exportmap = function (options = {}) {
           }
 
           // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-          var thisArg = arguments[1];
+          const thisArg = arguments[1];
 
           // 5. Let k be 0.
-          var k = 0;
+          let k = 0;
 
           // 6. Repeat, while k < len
           while (k < len) {
@@ -165,12 +176,12 @@ const Exportmap = function (options = {}) {
             // b. Let kValue be ? Get(O, Pk).
             // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
             // d. If testResult is true, return kValue.
-            var kValue = o[k];
+            const kValue = o[k];
             if (predicate.call(thisArg, kValue, k, o)) {
               return kValue;
             }
             // e. Increase k by 1.
-            k++;
+            k += 1;
           }
 
           // 7. Return undefined.
@@ -180,11 +191,11 @@ const Exportmap = function (options = {}) {
         writable: true
       });
     }
-  })();
+  }());
 
-  function createFormatsContainer(options) {
+  function createFormatsContainer(opts) {
     const el = `
-      <div id="${options.id}" class="folded">
+      <div id="${opts.id}" class="folded">
         <span id="o-export-format-png" class="o-export-format"> PNG </span>
         <span id="o-export-format-jpg" class="o-export-format"> JPG </span>
       </div>`;
@@ -219,10 +230,10 @@ const Exportmap = function (options = {}) {
     const formatsContainerId = 'o-select-format-button';
     const el = document.getElementById(id).parentElement;
     const formatsContainerString = createFormatsContainer({
-      id: formatsContainerId,
+      id: formatsContainerId
     });
-    const formatsContainerElement = dom.html(formatsContainerString)
-    //to support IE otherwise el.after(formatsContainerElement) would work perfectly.
+    const formatsContainerElement = dom.html(formatsContainerString);
+    // to support IE otherwise el.after(formatsContainerElement) would work perfectly.
     el.parentNode.insertBefore(formatsContainerElement, el.nextSibling);
     el.appendChild(formatsContainerElement);
     // dom.html method used above doesn't create a real element. To be able to use classList we need to use getElementBtId
@@ -231,7 +242,7 @@ const Exportmap = function (options = {}) {
       fc.classList.add('unfolded');
       fc.classList.remove('folded');
     }, 100);
-    
+
     const pngButton = document.getElementById('o-export-format-png');
     const jpgButton = document.getElementById('o-export-format-jpg');
 
@@ -247,7 +258,7 @@ const Exportmap = function (options = {}) {
       toggleFormatContainter();
       e.stopPropagation();
     });
-    
+
     alreadyDrawn = true;
     formatsContainerVisible = true;
   }
