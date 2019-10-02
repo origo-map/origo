0298499b895eb7ad9995529a2f289f5dbd56877a
Jonas <jonas.kumlin@karlstad.se>
2019-10-02 10:12:00 +0200

    Fixes #670
    Different appraoch than #671

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,22 +9,9 @@ export default function Footer(options = {}) {
   return Component({
     render: function render() {
       let middleContent = '';
-      if (data.img && !data.url && !data.urlText) {
-        middleContent = `<img src="${data.img}">`;
-      } else if (data.img && data.url && !data.urlText) {
-        middleContent = `<a href="${data.url}"><img src="${data.img}"></a>`;
-      } else if (data.img && !data.url && data.urlText) {
-        middleContent = `<img src="${data.img}">${data.urlText}`;
-      } else if (data.img && data.url && data.urlText) {
-        middleContent = `<img src="${data.img}"><a href="${data.url}">${data.urlText}</a>`;
-      } else if (data.img) {
-        middleContent = `<img src="${data.img}">`;
-      } else if (data.url && data.urlText) {
-        middleContent = `<a href="${data.url}">${data.urlText}</a>`;
-      } else if (data.text) {
-        middleContent = `<p>${data.text}</p>`;
-      }
-
+      middleContent += data.img ? `<img src="${data.img}" />` : '';
+      middleContent += data.text ? `<p>${data.text}</p>` : '';
+      middleContent = data.url ? `<a href="${data.url}">${middleContent}</a>` : middleContent;
       return `<div id=${this.getId()} class="o-footer relative ${cls}">
                 <div id="o-console" class="o-footer-left">&nbsp;</div>
                 <div class="o-footer-middle">
