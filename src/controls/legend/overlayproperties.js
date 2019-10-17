baed26c5fbe61887afa0c1c4ac5710d14339da2e
Jonas <jonas.kumlin@karlstad.se>
2019-10-17 12:50:59 +0200

    Some css

--- src/controls/legend/overlayproperties.js
+++ src/controls/legend/overlayproperties.js
@@ -12,10 +12,12 @@ const OverlayProperties = function OverlayProperties(options = {}) {
   const title = layer.get('title') || '';
   const abstract = layer.get('abstract') || '';
   const opacity = layer.getOpacity();
+  const opacityControl = layer.get('opacityControl') !== false;
   const style = viewer.getStyle(layer.get('styleName'));
   const legend = Legend(style, opacity);
   let overlayEl;
   let sliderEl;
+  const inputRange = opacityControl ? `<div class="padding-smaller"><input id="opacitySlider" type="range" min="0" max="1" value="${opacity}" step="0.1"><span class="text-smaller">0%</span><span style="float:right" class="text-smaller">100%</span></div>` : '';
 
   function extendedLegendZoom(e) {
     const parentOverlay = document.getElementById(options.parent.getId());
@@ -37,18 +39,20 @@ const OverlayProperties = function OverlayProperties(options = {}) {
     },
     onRender() {
       overlayEl = document.getElementById(this.getId());
-      sliderEl = overlayEl.getElementsByTagName('input')[0];
       overlayEl.addEventListener('click', (e) => {
         this.dispatch('click', e);
         e.preventDefault();
       });
-      sliderEl.addEventListener('input', (e) => {
-       layer.setOpacity(sliderEl.value);
-      });
+      if (opacityControl) {
+        sliderEl = document.getElementById('opacitySlider');
+        sliderEl.addEventListener('input', () => {
+          layer.setOpacity(sliderEl.value);
+        });
+      }
     },
     render() {
       return `<div id="${this.getId()}" class="${cls} border-bottom">
-                <div class="padding-small">${legend}<input type="range" min="0" max="1" value="${layer.getOpacity()}" step="0.05"></div>
+                <div class="padding-small">${legend}${inputRange}</div>
                 <p class="padding-bottom-small padding-x text-small">${abstract}</p>
               </div>`;
     },
