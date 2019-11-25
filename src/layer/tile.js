3eed4dc37961f9c7b91fe0a8884732e7ef2b765c
Jonas <jonas.kumlin@karlstad.se>
2019-11-25 11:24:21 +0100

    Added extent to options

--- src/layer/tile.js
+++ src/layer/tile.js
@@ -3,5 +3,6 @@ import TileLayer from 'ol/layer/Tile';
 export default function tile(options, source) {
   const opt = options;
   opt.source = source;
+  opt.extent = source.tileGrid.extent || undefined;
   return new TileLayer(opt);
 }
