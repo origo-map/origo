d4c71911d66f7ebe5963198e26c403c9d101c6a5
Iman Tahriri <iman.tahriri@decerno.se>
2020-03-01 17:02:52 +0100

    Adding changes to featureinfo.js

--- src/featureinfo.js
+++ src/featureinfo.js
@@ -10,6 +10,7 @@ import Style from './style';
 import StyleTypes from './style/styletypes';
 import getFeatureInfo from './getfeatureinfo';
 import replacer from '../src/utils/replacer';
+import SelectedItem from './models/SelectedItem';
 import { getContent } from './getattributes';
 
 const styleTypes = StyleTypes();
@@ -24,7 +25,6 @@ const Featureinfo = function Featureinfo(options = {}) {
     savedPin: savedPinOptions,
     savedSelection,
     selectionStyles: selectionStylesOptions,
-    showOverlay = true
   } = options;
 
   let identifyTarget;
@@ -33,66 +33,85 @@ const Featureinfo = function Featureinfo(options = {}) {
   let popup;
   let selectionLayer;
   let viewer;
+  let selectionManager;
 
   const pinStyle = Style.createStyleRule(pinStyleOptions)[0];
   const selectionStyles = selectionStylesOptions ? Style.createGeometryStyle(selectionStylesOptions) : Style.createEditStyle();
   let savedPin = savedPinOptions ? maputils.createPointFeature(savedPinOptions, pinStyle) : undefined;
   const savedFeature = savedPin || savedSelection || undefined;
+  const infowindow = 'infowindow' in options ? options.infowindow : 'overlay';
 
-  function setUIoutput(v) {
-    if (showOverlay) {
-      identifyTarget = 'overlay';
-    } else {
-      sidebar.init(v);
-      identifyTarget = 'sidebar';
-    }
-  }
+  identifyTarget = infowindow;
+
+  // function setUIoutput(v) {
+  //   if (showOverlay) {
+  //     identifyTarget = 'overlay';
+  //   } else {
+  //     sidebar.init(v);
+  //     identifyTarget = 'sidebar';
+  //   }
+  // }
 
   const clear = function clear() {
     selectionLayer.clear();
+    selectionManager.clearSelection();
     sidebar.setVisibility(false);
     if (overlay) {
       viewer.removeOverlays(overlay);
     }
   };
 
+  // TODO: direct access to feature and layer should be converted to getFeature and getLayer methods on currentItem
   const callback = function callback(evt) {
-    const currentItem = evt.item.index;
-    if (currentItem !== null) {
-      const clone = items[currentItem].feature.clone();
-      clone.setId(items[currentItem].feature.getId());
-      clone.layerName = items[currentItem].name;
+    const currentItemIndex = evt.item.index;
+    if (currentItemIndex !== null) {
+      let currentItem = items[currentItemIndex];
+      const clone = currentItem.feature.clone();
+      clone.setId(currentItem.feature.getId());
+      clone.layerName = currentItem.name;
+
       selectionLayer.clearAndAdd(
         clone,
-        selectionStyles[items[currentItem].feature.getGeometry().getType()]
+        selectionStyles[currentItem.feature.getGeometry().getType()]
       );
       let featureinfoTitle;
       let title;
       let layer;
-      if (items[currentItem].layer) {
-        if (typeof items[currentItem].layer === 'string') {
-          layer = viewer.getLayer(items[currentItem].layer);
+
+      if (currentItem.layer) {
+        if (typeof currentItem.layer === 'string') {
+          // bcuz in getfeatureinfo -> getFeaturesFromRemote only name of the layer is set on the object! (old version before using SelectedItems class)
+          layer = viewer.getLayer(currentItem.layer);
         } else {
-          layer = viewer.getLayer(items[currentItem].layer.get('name'));
+          layer = viewer.getLayer(currentItem.layer.get('name'));
         }
       }
+      // This is very strange: layer above is only a string, could not possibly have method.
       if (layer) {
         featureinfoTitle = layer.getProperties().featureinfoTitle;
       }
 
       if (featureinfoTitle) {
-        const featureProps = items[currentItem].feature.getProperties();
+        const featureProps = currentItem.feature.getProperties();
         title = replacer.replace(featureinfoTitle, featureProps);
         if (!title) {
-          title = items[currentItem].title ? items[currentItem].title : items[currentItem].name;
+          if (currentItem instanceof SelectedItem) {
+            title = currentItem.getLayer().get('title') ? currentItem.getLayer().get('title') : currentItem.getLayer().get('name');
+          } else {
+            title = currentItem.title ? currentItem.title : currentItem.name;
+          }
         }
       } else {
-        title = items[currentItem].title ? items[currentItem].title : items[currentItem].name;
+        if (currentItem instanceof SelectedItem) {
+          title = currentItem.getLayer().get('title') ? currentItem.getLayer().get('title') : currentItem.getLayer().get('name');
+        } else {
+          title = currentItem.title ? currentItem.title : currentItem.name;
+        }
       }
-      selectionLayer.setSourceLayer(items[currentItem].layer);
+      selectionLayer.setSourceLayer(currentItem.layer);
       if (identifyTarget === 'overlay') {
         popup.setTitle(title);
-      } else {
+      } else if (identifyTarget === 'sidebar') {
         sidebar.setTitle(title);
       }
     }
@@ -156,15 +175,15 @@ const Featureinfo = function Featureinfo(options = {}) {
       let modalStyle = '';
       switch (targ.target) {
         case 'modal-full':
-        {
-          modalStyle = 'max-width:unset;width:98%;height:98%;resize:both;overflow:auto;display:flex;flex-flow:column;';
-          break;
-        }
+          {
+            modalStyle = 'max-width:unset;width:98%;height:98%;resize:both;overflow:auto;display:flex;flex-flow:column;';
+            break;
+          }
         default:
-        {
-          modalStyle = 'resize:both;overflow:auto;display:flex;flex-flow:column;';
-          break;
-        }
+          {
+            modalStyle = 'resize:both;overflow:auto;display:flex;flex-flow:column;';
+            break;
+          }
       }
       Modal({
         title: targ.href,
@@ -184,61 +203,70 @@ const Featureinfo = function Featureinfo(options = {}) {
     content = '<div id="o-identify"><div id="o-identify-carousel" class="owl-carousel owl-theme"></div></div>';
     switch (target) {
       case 'overlay':
-      {
-        popup = Popup(`#${viewer.getId()}`);
-        popup.setContent({
-          content,
-          title: items[0].title
-        });
-        const contentDiv = document.getElementById('o-identify-carousel');
-        items.forEach((item) => {
-          if (item.content instanceof Element) {
-            contentDiv.appendChild(item.content);
-          } else {
-            contentDiv.innerHTML = item.content;
-          }
-        });
-        popup.setVisibility(true);
-        initCarousel('#o-identify-carousel');
-        const popupHeight = $('.o-popup').outerHeight() + 20;
-        $('#o-popup').height(popupHeight);
-        overlay = new Overlay({
-          element: popup.getEl(),
-          autoPan: true,
-          autoPanAnimation: {
-            duration: 500
-          },
-          autoPanMargin: 40,
-          positioning: 'bottom-center'
-        });
-        const geometry = items[0].feature.getGeometry();
-        const coord = geometry.getType() === 'Point' ? geometry.getCoordinates() : coordinate;
-        map.addOverlay(overlay);
-        overlay.setPosition(coord);
-        break;
-      }
+        {
+          popup = Popup(`#${viewer.getId()}`);
+          popup.setContent({
+            content,
+            title: items[0] instanceof SelectedItem ? items[0].getLayer().get('title') : items[0].title
+          });
+          const contentDiv = document.getElementById('o-identify-carousel');
+          items.forEach((item) => {
+            if (item.content instanceof Element) {
+              contentDiv.appendChild(item.content);
+            } else {
+              contentDiv.innerHTML = item.content;
+            }
+          });
+          popup.setVisibility(true);
+          initCarousel('#o-identify-carousel');
+          const popupHeight = $('.o-popup').outerHeight() + 20;
+          $('#o-popup').height(popupHeight);
+          overlay = new Overlay({
+            element: popup.getEl(),
+            autoPan: true,
+            autoPanAnimation: {
+              duration: 500
+            },
+            autoPanMargin: 40,
+            positioning: 'bottom-center'
+          });
+          const geometry = items[0].feature.getGeometry();
+          const coord = geometry.getType() === 'Point' ? geometry.getCoordinates() : coordinate;
+          map.addOverlay(overlay);
+          overlay.setPosition(coord);
+          break;
+        }
       case 'sidebar':
-      {
-        sidebar.setContent({
-          content,
-          title: items[0].title
-        });
-        const contentDiv = document.getElementById('o-identify-carousel');
-        items.forEach((item) => {
-          if (item.content instanceof Element) {
-            contentDiv.appendChild(item.content);
-          } else {
-            contentDiv.innerHTML = item.content;
+        {
+          sidebar.setContent({
+            content,
+            title: items[0] instanceof SelectedItem ? items[0].getLayer().get('title') : items[0].title
+          });
+          const contentDiv = document.getElementById('o-identify-carousel');
+          items.forEach((item) => {
+            if (item.content instanceof Element) {
+              contentDiv.appendChild(item.content);
+            } else {
+              contentDiv.innerHTML = item.content;
+            }
+          });
+          sidebar.setVisibility(true);
+          initCarousel('#o-identify-carousel');
+          break;
+        }
+      case 'infowindow':
+        {
+          if (items.length === 1) {
+            selectionManager.addOrHighlightItem(items[0]);
+          } else if (items.length > 1) {
+            selectionManager.addItems(items);
           }
-        });
-        sidebar.setVisibility(true);
-        initCarousel('#o-identify-carousel');
-        break;
-      }
+          break;
+        }
       default:
-      {
-        break;
-      }
+        {
+          break;
+        }
     }
 
     const modalLinks = document.getElementsByClassName('o-identify-link-modal');
@@ -275,7 +303,7 @@ const Featureinfo = function Featureinfo(options = {}) {
           if (result.length > 0) {
             selectionLayer.clear();
             render(result, identifyTarget, evt.coordinate);
-          } else if (selectionLayer.getFeatures().length > 0) {
+          } else if (selectionLayer.getFeatures().length > 0 || (identifyTarget === 'infowindow' && selectionManager.getNumberOfSelectedItems() > 0)) {
             clear();
           } else if (pinning) {
             const resolution = map.getView().getResolution();
@@ -312,8 +340,12 @@ const Featureinfo = function Featureinfo(options = {}) {
     onAdd(e) {
       viewer = e.target;
       const map = viewer.getMap();
-      setUIoutput(viewer);
+      if (identifyTarget === 'sidebar') {
+        sidebar.init(viewer);
+      }
+      // setUIoutput(viewer);
       selectionLayer = featurelayer(savedFeature, map);
+      selectionManager = viewer.getSelectionManager();
       map.on(clickEvent, onClick);
       viewer.on('toggleClickInteraction', (detail) => {
         if ((detail.name === 'featureinfo' && detail.active) || (detail.name !== 'featureinfo' && !detail.active)) {
