fb44d41f1286bfd73956d0e48a21985911119089
Jonas <jonas.kumlin@karlstad.se>
2020-03-03 14:15:13 +0100

    Update webpack.prod.js

--- tasks/webpack.prod.js
+++ tasks/webpack.prod.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
