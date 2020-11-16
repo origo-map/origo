8915c20fc41b793cba075abb2600e9c642ea1337
Jonas <jonas.kumlin@karlstad.se>
2020-11-16 15:33:36 +0100

    extent and filter fix

--- src/getfeature.js
+++ src/getfeature.js
@@ -64,23 +64,42 @@ sourceType.WFS = function wfsSourceType(id, layer, serverUrl, extent) {
   const format = new GeoJSONFormat({
     geometryName
   });
-  const filter = replacer.replace(layer.get('filter'), window);
-  const layerExtent = layer.get('extent');
-  const minExtent = [Math.max(extent[0], layerExtent[0]),
-    Math.max(extent[1], layerExtent[1]),
-    Math.min(extent[2], layerExtent[2]),
-    Math.min(extent[3], layerExtent[3])];
-  if (!(minExtent[0] < minExtent[2] && minExtent[1] < minExtent[3])) {
-    return false;
+  let queryFilter = '';
+  if (!id) {
+    const filter = replacer.replace(layer.get('filter'), window);
+    const layerExtent = layer.get('extent');
+    let minExtent;
+    if (extent && layerExtent) {
+      minExtent = [Math.max(extent[0], layerExtent[0]),
+        Math.max(extent[1], layerExtent[1]),
+        Math.min(extent[2], layerExtent[2]),
+        Math.min(extent[3], layerExtent[3])];
+      if (!(minExtent[0] < minExtent[2] && minExtent[1] < minExtent[3])) {
+        return [];
+      }
+    } else if (extent) {
+      minExtent = extent;
+    } else if (layerExtent) {
+      minExtent = layerExtent;
+    }
+    if (filter) {
+      if (minExtent) {
+        queryFilter = layer.get('geometryName') ? `&CQL_FILTER=${filter} AND BBOX(${layer.get('geometryName')},${minExtent.toString()})` : `&CQL_FILTER=${filter}&BBOX=${minExtent.toString()}`;
+      } else {
+        queryFilter = `&CQL_FILTER=${filter}`;
+      }
+    } else if (minExtent) {
+      queryFilter = layer.get('geometryName') ? `&CQL_FILTER=BBOX(${layer.get('geometryName')},${minExtent.toString()})` : `&BBOX=${minExtent.toString()}`;
+    }
+  } else {
+    queryFilter = `&featureId=${id}`;
   }
-  const queryFilter = filter ? `&CQL_FILTER=${filter} AND BBOX(${layer.get('geometryName')},${minExtent.toString()})` : `&BBOX=${minExtent.toString()}`;
   const url = `${serverUrl}?`;
   const data = ['service=WFS',
     '&version=1.0.0',
     `&request=GetFeature&typeName=${layer.get('name')}`,
     '&outputFormat=json',
-    extent ? queryFilter : '',
-    id ? `&featureId=${id}` : ''
+    queryFilter
   ].join('');
 
   return $.ajax({
