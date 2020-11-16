4543d811c0dbf4f39db2b16be50244620fd3c71d
Jonas <jonas.kumlin@karlstad.se>
2020-11-16 12:51:38 +0100

    Consider extent

--- src/getfeature.js
+++ src/getfeature.js
@@ -1,18 +1,22 @@
 import EsriJSONFormat from 'ol/format/EsriJSON';
 import GeoJSONFormat from 'ol/format/GeoJSON';
 import $ from 'jquery';
+import replacer from './utils/replacer';
 
 let projectionCode;
 let projection;
 const sourceType = {};
 
-export default function (id, layer, source, projCode, proj) {
+export default function (id, layer, source, projCode, proj, extent) {
   projectionCode = projCode;
   projection = proj;
   const serverUrl = source[layer.get('sourceName')].url;
   const type = layer.get('type');
   // returns a promise with features as result
-  return sourceType[type](id, layer, serverUrl);
+  if (type === 'AGS_FEATURE') {
+    return sourceType.AGS_FEATURE(id, layer, serverUrl);
+  }
+  return sourceType.WFS(id, layer, serverUrl, extent);
 }
 
 function fail(response) {
@@ -55,23 +59,27 @@ sourceType.AGS_FEATURE = function agsFeature(id, layer, serverUrl) {
     }, fail);
 };
 
-sourceType.WFS = function wfsSourceType(id, layer, serverUrl) {
+sourceType.WFS = function wfsSourceType(id, layer, serverUrl, extent) {
   const geometryName = layer.get('geometryName');
   const format = new GeoJSONFormat({
     geometryName
   });
+  const filter = replacer.replace(layer.get('filter'), window);
+  const bbox = extent ? extent.toString() : '';
+  const layerExtent = layer.get('extent');
+  const queryFilter = filter ? `&CQL_FILTER=${filter} AND BBOX(${layer.get('geometryName')},${layerExtent})` : `&BBOX=${bbox}`;
   const url = `${serverUrl}?`;
   const data = ['service=WFS',
     '&version=1.0.0',
     `&request=GetFeature&typeName=${layer.get('name')}`,
     '&outputFormat=json',
-    `&featureId=${id}`
+    extent ? queryFilter : '',
+    id ? `&featureId=${id}` : ''
   ].join('');
-
   return $.ajax({
     url,
     data,
-    type: 'POST',
+    type: 'GET',
     dataType: 'json'
   })
     .then(response => format.readFeatures(response));
