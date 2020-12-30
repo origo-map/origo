9f615baa9cfb36b48ba569e4345226770c691b6f
Rasmussen <daniel.rasmussen@vasteras.se>
2020-12-30 11:01:10 +0100

    makes eslint loader optional

---
+++ tasks/webpack.lint.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.lint.common.js');
+
+module.exports = merge(common, {
+  output: {
+    publicPath: '/js',
+    filename: 'origo.js',
+    libraryTarget: 'var',
+    libraryExport: 'default',
+    library: 'Origo'
+  },
+  devServer: {
+    contentBase: './',
+    port: 9966
+  }
+});
