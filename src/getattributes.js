8d44a726b1b01c457809a18aed188267866dff51
Flodkvist <31692548+Flodkvist@users.noreply.github.com>
2021-01-11 22:31:07 +0100

    Adds attribute option for title of modal displaying iframe. (#1083)
    * Added settings for modal title bar.
    
    * Adds attribute option targetTitle

--- src/getattributes.js
+++ src/getattributes.js
@@ -26,6 +26,7 @@ const getContent = {
         } else if (isUrl(attribute.url)) {
           url = attribute.url;
         } else return false;
+        const aTargetTitle = replacer.replace(attribute.targetTitle, attributes) || url;
         let aTarget = '_blank';
         let aCls = 'o-identify-link';
         if (attribute.target === 'modal') {
@@ -35,7 +36,7 @@ const getContent = {
           aTarget = 'modal-full';
           aCls = 'o-identify-link-modal';
         }
-        val = `<a class="${aCls}" target="${aTarget}" href="${url}">${feature.get(attribute.name)}</a>`;
+        val = `<a class="${aCls}" target="${aTarget}" href="${url}" title="${aTargetTitle}">${feature.get(attribute.name)}</a>`;
       }
     }
     const newElement = document.createElement('li');
@@ -52,6 +53,7 @@ const getContent = {
       url = attribute.url;
     } else return false;
     const text = attribute.html || attribute.title || attribute.url;
+    const aTargetTitle = replacer.replace(attribute.targetTitle, attributes) || url;
     let aTarget = '_blank';
     let aCls = 'o-identify-link';
     if (attribute.target === 'modal') {
@@ -61,7 +63,7 @@ const getContent = {
       aTarget = 'modal-full';
       aCls = 'o-identify-link-modal';
     }
-    val = `<a class="${aCls}" target="${aTarget}" href="${url}">${text}</a>`;
+    val = `<a class="${aCls}" target="${aTarget}" href="${url}" title="${aTargetTitle}">${text}</a>`;
     const newElement = document.createElement('li');
     newElement.classList.add(attribute.cls);
     newElement.innerHTML = val;
