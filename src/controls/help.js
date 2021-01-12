1bf7d2537c08ec47d52ab39e0dd0e40851df0db9
Mattias Spångmyr <mattias.spangmyr@haninge.se>
2021-01-12 11:05:20 +0100

    Replaced eval() by referencing nested objects

--- src/controls/help.js
+++ src/controls/help.js
@@ -10,60 +10,109 @@ const Help = function Help(options = {}) {
   let menuItem;
   let modal;
   const contentItems = [];
-
-  const AboutText = 'Här finner du mer information';
-  const ZoomInText = 'Zooma in i kartan';
-  const ZoomOutText = 'Zooma ut i kartan';
-  const HelpText = 'Hjälper dig att hitta hit';
-  const HomeText = 'Tar kartan till startposition';
-  const GeopositionText = 'Tar kartan till din position';
-  const MeasureText = 'Mät avtånd och ytor';
-  const MapmenuText = 'I menyn hittar du fler verktyg';
-  const SharemapText = 'Dela din karta med en kompis!';
-  const PrintText = 'Skriv ut kartan till en pdf';
-  const LinkText = 'Tar dig någon annanstans...';
-  const DraganddropText = 'Du har möjlighet att dra in lager från din dator till kartan. Möjliga format är: GPX, GeoJSON, IGC, KML och TopoJSON';
-  const EditorText = 'Öppnar redigeringsverktyget';
-  const FullscreenText = 'Öppnar kartan i helskärmsläge';
-  const LegendText = 'Innehållsförteckning och teckenförklaring - visa eller dölj kartlager, klicka på rubriker och kartlager för information, ändra lagers genomskinlighet mm';
-  const PositionText = 'Klicka på denna om du vill ha positionen från kartans centrum och kan definiera egna koordinater direkt i koordinattexten. Byt system genom att klicka på koordinatsystemet';
-  const ProgressbarText = 'Den blå linjen i nedre delan av kartan visar på hämtning av information';
-  const RotateText = 'Rotera kartan med två fingrar på pekskärm, håll ned Shift+Alt på PC. Klicka på symbolen för att återgå';
-  const ScaleText = '';
-  const ScalelineText = '';
-  const SearchText = 'Sök i kartan';
-  const SplashText = '';
-  const ExternalurlText = 'Öppnar fler knappar där du kan välja att öppna din kartposition i annan applikation';
-  const ScalepickerText = 'Visar kartans nuvarande skala. Klicka för att välja skala manuellt';
-  const AboutIcon = '#ic_info_outline_24px';
-  const ZoomInIcon = '#ic_add_24px';
-  const ZoomOutIcon = '#ic_remove_24px';
-  const HelpIcon = '#ic_help_outline_24px';
-  const HomeIcon = '#ic_home_24px';
-  const GeopositionIcon = '#ic_near_me_24px';
-  const MeasureIcon = '#ic_straighten_24px';
-  const MapmenuIcon = '#ic_menu_24px';
-  const SharemapIcon = '#ic_screen_share_outline_24px';
-  const PrintIcon = '#ic_print_24px';
-  const LinkIcon = '#ic_launch_24px';
-  const DraganddropIcon = '';
-  const EditorIcon = '#ic_edit_24px';
-  const FullscreenIcon = '#ic_fullscreen_24px';
-  const LegendIcon = '#ic_layers_24px';
-  const PositionIcon = '#ic_gps_not_fixed_24px';
-  const ProgressbarIcon = '';
-  const RotateIcon = '#origo-compass';
-  const ScaleIcon = '';
-  const ScalelineIcon = '';
-  const SearchIcon = '#ic_search_24px';
-  const SplashIcon = '';
-  const ExternalurlIcon = '#ic_baseline_link_24px';
-  const ScalepickerIcon = '';
+  const defaultOptions = {
+    'About': {
+      'text': 'Här finner du mer information',
+      'icon': '#ic_info_outline_24px'
+    },
+    'ZoomIn': {
+      'text': 'Zooma in i kartan',
+      'icon': '#ic_add_24px'
+    },
+    'ZoomOut': {
+      'text': 'Zooma ut i kartan',
+      'icon': '#ic_remove_24px'
+    },
+    'Help': {
+      'text': 'Hjälper dig att hitta hit',
+      'icon': '#ic_help_outline_24px'
+    },
+    'Home': {
+      'text': 'Tar kartan till startposition',
+      'icon': '#ic_home_24px'
+    },
+    'Geoposition': {
+      'text': 'Tar kartan till din position',
+      'icon': '#ic_near_me_24px'
+    },
+    'Measure': {
+      'text': 'Mät avtånd och ytor',
+      'icon': '#ic_straighten_24px'
+    },
+    'Mapmenu': {
+      'text': 'I menyn hittar du fler verktyg',
+      'icon': '#ic_menu_24px'
+    },
+    'Sharemap': {
+      'text': 'Dela din karta med en kompis!',
+      'icon': '#ic_screen_share_outline_24px'
+    },
+    'Print': {
+      'text': 'Skriv ut kartan till en pdf',
+      'icon': '#ic_print_24px'
+    },
+    'Link': {
+      'text': 'Tar dig någon annanstans...',
+      'icon': '#ic_launch_24px'
+    },
+    'Draganddrop': {
+      'text': 'Du har möjlighet att dra in lager från din dator till kartan. Möjliga format är: GPX, GeoJSON, IGC, KML och TopoJSON',
+      'icon': ''
+    },
+    'Editor': {
+      'text': 'Öppnar redigeringsverktyget',
+      'icon': '#ic_edit_24px'
+    },
+    'Fullscreen': {
+      'text': 'Öppnar kartan i helskärmsläge',
+      'icon': '#ic_fullscreen_24px'
+    },
+    'Legend': {
+      'text': 'Innehållsförteckning och teckenförklaring - visa eller dölj kartlager, klicka på rubriker och kartlager för information, ändra lagers genomskinlighet mm',
+      'icon': '#ic_layers_24px'
+    },
+    'Position': {
+      'text': 'Klicka på denna om du vill ha positionen från kartans centrum och kan definiera egna koordinater direkt i koordinattexten. Byt system genom att klicka på koordinatsystemet',
+      'icon': '#ic_gps_not_fixed_24px'
+    },
+    'Progressbar': {
+      'text': 'Den blå linjen i nedre delan av kartan visar på hämtning av information',
+      'icon': ''
+    },
+    'Rotate': {
+      'text': 'Rotera kartan med två fingrar på pekskärm, håll ned Shift+Alt på PC. Klicka på symbolen för att återgå',
+      'icon': '#origo-compass'
+    },
+    'Scale': {
+      'text': '',
+      'icon': ''
+    },
+    'Scaleline': {
+      'text': '',
+      'icon': ''
+    },
+    'Search': {
+      'text': 'Sök i kartan',
+      'icon': '#ic_search_24px'
+    },
+    'Splash': {
+      'text': '',
+      'icon': ''
+    },
+    'Externalurl': {
+      'text': 'Öppnar fler knappar där du kan välja att öppna din kartposition i annan applikation',
+      'icon': '#ic_baseline_link_24px'
+    },
+    'Scalepicker': {
+      'text': 'Visar kartans nuvarande skala. Klicka för att välja skala manuellt',
+      'icon': ''
+    }
+  };
 
   const modalContent = () => {
     options.controlList.forEach((el) => {
-      const text = el.text || eval(`${el.name}Text`) || '';
-      const icon = el.icon || eval(`${el.name}Icon`) || '';
+      const text = el.text || defaultOptions[el.name].text;
+      const icon = el.icon || defaultOptions[el.name].icon;
       const iconStyle = el.iconStyle || '';
       const controlIcons = Icon({
         icon: icon,
