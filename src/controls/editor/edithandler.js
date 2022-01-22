6cf8a6dc9b52c15c8143588bab5e274126f32a4e
mulfvik <ulfvik.mikael@outlook.com>
2022-01-22 13:40:01 +0100

    Fix for input required

--- src/controls/editor/edithandler.js
+++ src/controls/editor/edithandler.js
@@ -609,7 +609,7 @@ function onAttributesSave(features, attrs) {
       const inputValue = document.getElementById(attribute.elId).value;
       const inputName = document.getElementById(attribute.elId).getAttribute('name');
       const inputId = document.getElementById(attribute.elId).getAttribute('id');
-      const inputRequired = document.getElementById(attribute.elId).getAttribute('required');
+      const inputRequired = document.getElementById(attribute.elId).required;
 
       // If hidden element it should be excluded
       // By sheer luck, this prevents attributes to be changed in batch edit mode when checkbox is not checked.
