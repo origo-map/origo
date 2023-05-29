b41031d4753211f0aa94f1604b56d21da4e1ec63
Johnny Blästa <johnny.blasta@sundsvall.se>
2023-05-29 13:29:56 +0200

    fix: let img attribute have precedence before name

--- src/getattributes.js
+++ src/getattributes.js
@@ -221,12 +221,12 @@ function getAttributes(feature, layer, map) {
           featureinfoElement.appendChild(templateList);
           templateList.innerHTML = li;
         } else {
-          if (attribute.name) {
+          if (attribute.img || attribute.type === 'image') {
+            val = getContent.img(feature, attribute, attributes, map);
+          } else if (attribute.name) {
             val = getContent.name(feature, attribute, attributes, map);
           } else if (attribute.url) {
             val = getContent.url(feature, attribute, attributes, map);
-          } else if (attribute.img || attribute.type === 'image') {
-            val = getContent.img(feature, attribute, attributes, map);
           } else if (attribute.html) {
             val = getContent.html(feature, attribute, attributes, map);
           } else if (attribute.carousel) {
