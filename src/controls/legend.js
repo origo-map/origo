39a43aa294d06d181f64c9f5832ff26dfa94ee78
Jonas <jonas.kumlin@karlstad.se>
2023-12-04 14:10:04 +0100

    feature: new group options (#1910)
    * New options for groups
    
    * Lint
    
    * Go through subgroups as well
    
    * Works for groups as well
    
    * decription and abstract
    
    Added description for layer and option to show abstract for groups on properties page as well. This way layer and groups behave quite similar.
    
    * fix
    
    * showAbstractInLegend option

--- src/controls/legend.js
+++ src/controls/legend.js
@@ -555,6 +555,7 @@ const Legend = function Legend(options = {}) {
     },
     getuseGroupIndication() { return useGroupIndication; },
     getOverlaysCollapse() { return overlaysCmp.overlaysCollapse; },
+    getOverlays() { return overlaysCmp; },
     setVisibleLayersViewActive,
     addButtonToTools(button, buttonGroup) {
       if (buttonGroup === 'addLayerButton') {
