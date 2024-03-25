75f65ce13a748be5a1fca7ac7796023a0c567f2b
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-03-25 12:53:52 +0100

    Update legend.js

--- src/controls/legend.js
+++ src/controls/legend.js
@@ -698,7 +698,7 @@ const Legend = function Legend(options = {}) {
         cls: 'flex padding-small no-shrink',
         style: {
           'background-color': '#fff',
-          'max-width: '300px',
+          'max-width': '300px',
           'border-top': '1px solid #dbdbdb',
           'border-radius': '0.5rem'
         },
