1716bc0b945c94acacc9a3e95a30e61006d07957
Johnny Blästa <johnny.blasta@sundsvall.se>
2025-10-13 14:07:03 +0200

    make attachment handle nested foreignKey

--- src/utils/attachmentclient.js
+++ src/utils/attachmentclient.js
@@ -51,7 +51,28 @@ const attachmentclient = function attachmentclient(layer) {
    */
   function getId(feature) {
     if (config.foreignKey) {
-      return encodeURIComponent(feature.get(config.foreignKey));
+      let foreignKeyValue = '';
+      // Handle if foreignKey is nested json object
+      if (typeof feature.get(config.foreignKey) === 'undefined' && config.foreignKey.indexOf('.') > 0) {
+        const splitMatch = config.foreignKey.split('.');
+        let key = splitMatch.shift(); // Get the first key
+        let objectTemp = feature.get(key); // Get the initial object
+
+        // Drill down to the end of the nested attribute and return the value
+        while (splitMatch.length > 0 && objectTemp !== undefined) {
+          key = splitMatch.shift(); // Get the next key
+          if (objectTemp && key in objectTemp) {
+            objectTemp = objectTemp[key]; // Move deeper into the object
+          } else {
+            objectTemp = undefined; // Key does not exist, set to undefined
+            break; // No need to continue if the key is not found
+          }
+        }
+        foreignKeyValue = objectTemp; // Assign the nested value to featureValue
+      } else {
+        foreignKeyValue = feature.get(config.foreignKey);
+      }
+      return encodeURIComponent(foreignKeyValue);
     }
     let id = feature.getId();
     if (stripLayerNameFromId) {
