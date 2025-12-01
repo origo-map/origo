56a528de8c3f0fa49852959788430e9aac85a226
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-12-01 19:14:58 +0100

    Refactor loadresources.js to simplify permalink handling
    Removed getLoadMapStateIdMethod function and updated permalink logic to directly use loadMapStateIdMethod from controls.

--- src/loadresources.js
+++ src/loadresources.js
@@ -5,14 +5,6 @@ import isUrl from './utils/isurl';
 import trimUrl from './utils/trimurl';
 import stripJSONComments from './utils/stripjsoncomments';
 
-function getLoadMapStateIdMethod(config) {
-  const sharemapControl = config.controls.find(control => control.name === 'sharemap');
-  if (sharemapControl && sharemapControl.options && sharemapControl.options.loadMapStateIdMethod) {
-    return sharemapControl.options.loadMapStateIdMethod;
-  }
-  return 'path';
-}
-
 function getQueryVariable(variable, storeMethod) {
   const query = window.location.search.substring(1);
   const vars = query.split('&');
@@ -83,7 +75,6 @@ const loadResources = async function loadResources(mapOptions, config) {
           }
         });
       }
-      permalink.setLoadMapStateIdMethod(getLoadMapStateIdMethod(map.options));
       map.options.url = getUrl();
       map.options.map = undefined;
       map.options.params = urlParams;
@@ -91,6 +82,10 @@ const loadResources = async function loadResources(mapOptions, config) {
         if (map.options.controls[i].name === 'sharemap'
             && map.options.controls[i].options?.storeMethod === 'saveStateToServer') {
           storeMethod = 'saveStateToServer';
+          const loadMapStateIdMethod = map.options.controls[i].options?.loadMapStateIdMethod;
+          if (loadMapStateIdMethod) {
+            permalink.setLoadMapStateIdMethod(loadMapStateIdMethod);
+          }
           permalink.setSaveOnServerServiceEndpoint(map.options.controls[i].options.serviceEndpoint);
         }
       }
