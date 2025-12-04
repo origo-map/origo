ec75a5575c336277cfa3d88f78132100a552b867
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-12-04 10:25:54 +0100

    Fix for external config

--- src/loadresources.js
+++ src/loadresources.js
@@ -49,6 +49,7 @@ const loadResources = async function loadResources(mapOptions, config) {
   const mapEl = config.target;
   const format = 'json';
   let storeMethod = 'default';
+  let loadMapStateIdMethod;
   let urlParams;
   let url;
   let mapUrl;
@@ -82,7 +83,7 @@ const loadResources = async function loadResources(mapOptions, config) {
         if (map.options.controls[i].name === 'sharemap'
             && map.options.controls[i].options?.storeMethod === 'saveStateToServer') {
           storeMethod = 'saveStateToServer';
-          const loadMapStateIdMethod = map.options.controls[i].options?.loadMapStateIdMethod;
+          loadMapStateIdMethod = map.options.controls[i].options?.loadMapStateIdMethod;
           if (loadMapStateIdMethod) {
             permalink.setLoadMapStateIdMethod(loadMapStateIdMethod);
           }
@@ -177,6 +178,10 @@ const loadResources = async function loadResources(mapOptions, config) {
                   const options = map.options.controls[i].options;
                   if (options.storeMethod && options.storeMethod === 'saveStateToServer') {
                     storeMethod = options.storeMethod;
+                    loadMapStateIdMethod = options.loadMapStateIdMethod;
+                    if (loadMapStateIdMethod) {
+                      permalink.setLoadMapStateIdMethod(loadMapStateIdMethod);
+                    }
                     permalink.setSaveOnServerServiceEndpoint(map.options.controls[i].options.serviceEndpoint);
                   }
                 }
